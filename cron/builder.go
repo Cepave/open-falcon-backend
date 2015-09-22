1591a6d211ee667f3a19d72eb47f60d777cd3fe9
myhung <hungming8168@gmail.com>
2015-09-22 13:11:11 +0000

    Update mail html style.

--- cron/builder.go
+++ cron/builder.go
@@ -27,59 +27,63 @@ func BuildCommonSMSContent(event *model.Event) string {
 
 func BuildCommonMailContent(event *model.Event) string {
 	link := g.Link(event)
+	tdtl := `style="border: 1px solid #ccc; background: #FFF4F4;"`
+	tdtr := `style="border: 1px solid #ccc; border-left: none;"`
+	tdl := `style="border: 1px solid #ccc; border-top: none; background: #FFF4F4;"`
+	tdr := `style="border: 1px solid #ccc; border-top: none; border-left: none;"`
 	return fmt.Sprintf(
-		`<html><body><table border="1" cellpadding="10" cellspacing="0" style="border: 1px solid #ccc;">
- 			<tr style="border: 1px solid #ccc;">
-    				<td colspan="row">%s</td>
-    				<td colspan="row">%d</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Endpoint:</td>
-    				<td scope="row">%s</td>
-			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Metric:</td>
-    				<td align="left">%s</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Tags:</td>
-    				<td align="left">%s</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">%s</td>
-    				<td align="left">%s%s%s</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Note:</td>
-    				<td align="left">%s</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Max:</td>
-    				<td align="left">%d</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Current:</td>
-    				<td align="left">%d</td>
-  			</tr>
-  			<tr style="border: 1px solid #ccc;">
-    				<td scope="row">Timesramp:</td>
-    				<td align="left">%s</td>
-  			</tr>
-		</table>
-		<br>
-		<a href="%s">%s</a></body></html>`,
-		event.Status,
-		event.Priority(),
-		event.Endpoint,
-		event.Metric(),
-		utils.SortedTags(event.PushedTags),
-		event.Func(),
-		utils.ReadableFloat(event.LeftValue),
-		event.Operator(),
-		utils.ReadableFloat(event.RightValue()),
-		event.Note(),
-		event.MaxStep(),
-		event.CurrentStep,
-		event.FormattedTime(),
+		`<html><head><meta charset="utf-8"></head>
+		<body>
+			<table border="0" cellpadding="5" cellspacing="0">
+                                <tr>
+                                        <td %s >%s</td>
+                                        <td %s >%d</td></tr>
+                                <tr>
+                                        <td %s>Endpoint:</td>
+                                        <td %s>%s</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Metric:</td>
+                                        <td %s>%s</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Tags:</td>
+                                        <td %s>%s</td>
+                                </tr>
+                                <tr>
+                                        <td %s>%s</td>
+                                        <td %s>%s%s%s</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Note:</td>
+                                        <td %s>%s</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Max:</td>
+                                        <td %s>%d</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Current:</td>
+                                        <td %s>%d</td>
+                                </tr>
+                                <tr>
+                                        <td %s>Timesramp:</td>
+                                        <td %s>%s</td>
+                                </tr>
+                        </table>
+			<br>
+			<a href="%s">%s</a>
+		</body></html>`,
+
+		tdtl, event.Status, tdtr, event.Priority(),
+		tdl, tdr, event.Endpoint,
+		tdl, tdr, event.Metric(),
+		tdl, tdr, utils.SortedTags(event.PushedTags),
+		tdl, event.Func(), tdr, utils.ReadableFloat(event.LeftValue), event.Operator(),	utils.ReadableFloat(event.RightValue()),
+		tdl, tdr, event.Note(),
+		tdl, tdr, event.MaxStep(),
+		tdl, tdr, event.CurrentStep,
+		tdl, tdr, event.FormattedTime(),
 		link,
 		link,
 	)
