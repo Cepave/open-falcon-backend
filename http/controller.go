7773c1980b7f8e770b57c5db074212be745928fe
Kordan <kordan@hitripod.com>
2015-11-02 11:57:53 +0800

    If the debug option is ON, skip the login check.

--- http/controller.go
+++ http/controller.go
@@ -124,11 +124,13 @@ func CheckLoginStatusByCookie(sig string) bool {
 
 func (this *MainController) Index() {
 
-	sig := this.Ctx.GetCookie("sig")
-	isLoggedIn := CheckLoginStatusByCookie(sig)
-	if !isLoggedIn {
-		RedirectUrl := g.Config().RedirectUrl
-		this.Redirect(RedirectUrl, 302)
+	if false == g.Config().Debug {
+		sig := this.Ctx.GetCookie("sig")
+		isLoggedIn := CheckLoginStatusByCookie(sig)
+		if !isLoggedIn {
+			RedirectUrl := g.Config().RedirectUrl
+			this.Redirect(RedirectUrl, 302)
+		}
 	}
 
 	events := g.Events.Clone()
