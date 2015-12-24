e9050356b785da43c303db1e344248d482d11157
chyeh <chyeh@cepave.com>
2015-12-24 14:25:22 +0800

    Transform parameters in JSON format
    Create a type `ParamToAgent` as the type of the variables that save the
    data to push. Then transform these variables to parameters in JSON
    format.

--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
-import "fmt"
+import (
+	"fmt"
+//	"net/http"
+	"encoding/json"
+	"time"
+)
+
+type ParamToAgent struct {
+    Metric      string      `json:"metric"`
+    Endpoint    string      `json:"endpoint"`
+    Value       interface{} `json:"value"`       // number or string
+    CounterType string      `json:"counterType"`
+    Tags        string      `json:"tags"`
+    Timestamp   int64       `json:"timestamp"`
+    Step        int64       `json:"step"`
+}
 func main() {
-    fmt.Println("hello world")
+    urlPush := "http://172.17.0.11:1988/v1/push"
+	params := make([]*ParamToAgent, 0)
+    metric := "transmission-time"
+    endpoint := "nqm-endpoint"
+    value := 100.0
+    counterType := "GAUGE"
+    tags := "view=1,ip=2.3.4.5"
+    timestamp := time.Now().Unix()
+    step := int64(30)
+	
+    params = append(params, &ParamToAgent{metric, endpoint, value, counterType, tags, timestamp, step})
+    paramBody, err := json.Marshal(params)
+    //if err != nil {
+        fmt.Println(urlPush+", format body error,", err)
+        
+	//}
+
+    fmt.Println("URL:>", urlPush)
+    fmt.Println("URL:>", string(paramBody))
 }
