d1e00151d18a0b0e8eade323260f100ef7d97e48
chyeh <chyeh@cepave.com>
2015-12-24 17:47:02 +0800

    Get the results from `fping` and push to agent
    Create a type `SamplingIPAddress` as the type of the sampling frame.
    Execute the command `fping` and get the results. Note that the result
    is directed to `STDERR` by default. Parse the results into a string
    slice. Form the JSOM parameters by the parsed fields.

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"time"
     "bytes"
+	"os/exec"
+    "strings"
+    "strconv"
 )
 
 type ParamToAgent struct {
@@ -16,18 +19,43 @@ type ParamToAgent struct {
     Timestamp   int64       `json:"timestamp"`
     Step        int64       `json:"step"`
 }
+
+type SamplingIPAddress struct {
+    Address    string
+    View       int
+}
+
 func main() {
     urlPush := "http://10.20.30.40:1988/v1/push"
 	params := make([]*ParamToAgent, 0)
+	samplingFrame := make([]*SamplingIPAddress, 0)
+    
+    samplingFrame = append(samplingFrame, &SamplingIPAddress{"8.8.8.8", 2})
+	fpingCommand := exec.Command("fping", "-p", "20", "-i", "10", "-c", "4", "-q", "-a", "8.8.8.8")
+    cmdOutput, err := fpingCommand.CombinedOutput()
+    fpingResult := string(cmdOutput)
+    fmt.Print(fpingResult)
+    
+    parsedFpingResult := strings.FieldsFunc(fpingResult, func(r rune) bool {
+        switch r {
+            case ':', '/', '=', ',':
+                return true
+        }
+        return false
+    })
+    fmt.Println(parsedFpingResult)
+    view := strconv.Itoa(samplingFrame[0].View)
+    
     metric := "transmission-time"
     endpoint := "nqm-endpoint"
-    value := 100.0
+    value := parsedFpingResult[11]
     counterType := "GAUGE"
-    tags := "view=1,ip=2.3.4.5"
+    tags := "view="+view+",ip="+samplingFrame[0].Address
     timestamp := time.Now().Unix()
     step := int64(30)
 	
     params = append(params, &ParamToAgent{metric, endpoint, value, counterType, tags, timestamp, step})
+
     
     paramsBody, err := json.Marshal(params)
     if err != nil {
