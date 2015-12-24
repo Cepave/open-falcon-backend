5865c713652ec7140fe0c49b7f941f86f4b25962
chyeh <chyeh@cepave.com>
2015-12-24 15:13:58 +0800

    Push parameters in JSON format
    Send HTTP POST requests to push parameters to Open-Falcon agents.

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 import (
 	"fmt"
-//	"net/http"
+	"net/http"
 	"encoding/json"
 	"time"
+    "bytes"
 )
 
 type ParamToAgent struct {
@@ -16,7 +17,7 @@ type ParamToAgent struct {
     Step        int64       `json:"step"`
 }
 func main() {
-    urlPush := "http://172.17.0.11:1988/v1/push"
+    urlPush := "http://10.20.30.40:1988/v1/push"
 	params := make([]*ParamToAgent, 0)
     metric := "transmission-time"
     endpoint := "nqm-endpoint"
@@ -27,12 +28,23 @@ func main() {
     step := int64(30)
 	
     params = append(params, &ParamToAgent{metric, endpoint, value, counterType, tags, timestamp, step})
-    paramBody, err := json.Marshal(params)
-    //if err != nil {
-        fmt.Println(urlPush+", format body error,", err)
-        
-	//}
+    
+    paramsBody, err := json.Marshal(params)
+    if err != nil {
+        fmt.Println(urlPush+", format body error,", err)    
+	}
 
-    fmt.Println("URL:>", urlPush)
-    fmt.Println("URL:>", string(paramBody))
+	postReq, err := http.NewRequest("POST", urlPush, bytes.NewBuffer(paramsBody))
+	postReq.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	postReq.Header.Set("Connection", "close")
+	
+    httpClient := &http.Client{}
+    postResp, err := httpClient.Do(postReq)
+	if err != nil {
+		fmt.Println(urlPush+", sending post request occurs error,", err)
+	}
+	defer postResp.Body.Close()
+    
+    fmt.Println("URL:", urlPush)
+    fmt.Println("Body:", string(paramsBody))
 }
