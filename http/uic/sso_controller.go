1b085cc6fe20cc8e9d7ea26986e28757b638c17f
Minimum <s5550055@yahoo.com.tw>
2016-01-25 11:42:42 +0000

    Fix compiling error because beego has new version.

--- http/uic/sso_controller.go
+++ http/uic/sso_controller.go
@@ -43,7 +43,7 @@ func (this *SsoController) User() {
 	this.Data["json"] = map[string]interface{}{
 		"user": u,
 	}
-	this.ServeJson()
+	this.ServeJSON()
 }
 
 func (this *SsoController) Logout() {
