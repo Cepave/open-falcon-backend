d817c8354b608854519bbb88579bd67e506b3098
Mike Lue <mikelue@cepave.com>
2016-01-25 11:59:45 +0800

    [OWL-281] Add go source code for DB Patch

---
+++ dbpatch/go/dbpatch_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"testing"
+	"github.com/mikelue/owl-dbpatch/changelog"
+	. "gopkg.in/check.v1"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type DefaultSuite struct{}
+
+var _ = Suite(&DefaultSuite{})
+
+/**
+ * Tests the checking of configuration
+ */
+func (s *DefaultSuite) TestCheckRunPatchConfig(c *C) {
+	var testCases = [][]interface{} {
+		{ "mysql", "msyql://localhost/", "changeLog.json", "patch-files", true }, // Passed check
+		{ "", "msyql://localhost/", "changeLog.json", "patch-files", false }, // Failed check
+		{ "mysql", "", "changeLog.json", "patch-files", false }, // Failed check
+	}
+
+	for _, testCase := range testCases {
+		var testedResult = checkRunPatchConfig(&changelog.ChangeLogConfig{
+			DriverName: testCase[0].(string),
+			Dsn: testCase[1].(string),
+			ChangeLog: testCase[2].(string),
+			PatchFileBase: testCase[3].(string),
+		})
+
+		c.Assert(testedResult, Equals, testCase[4].(bool))
+	}
+}
