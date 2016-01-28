04a8e2467277ac9fbaf2922909da266cdb1e7b52
chyeh <chyeh@cepave.com>
2016-01-28 16:12:48 +0800

    Support getting configurations from `hbs`
    By RPC connection, it sends requests to get the the information of
    targets to probe. Notice that the `connectionId` is a property which
    tells the module `hbs` who you are. It has a default value of
    nqm-agent-1@10.20.30.40. You can also set the value by:
    
        ./nqm/nqm -connectionId <The Connection ID>

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 	"os/exec"
     "strings"
     "strconv"
+    "github.com/Cepave/common/model"
+    "net/rpc"
+    "sync"
+    "github.com/toolkits/net"
+    "log"
+    "math"
+    "flag"
 )
 
 type ParamToAgent struct {
@@ -41,27 +48,64 @@ type Statistic struct {
     Value          interface{}
 }
 
-func main() {
-    urlPush := "http://10.20.30.40:1988/v1/push"
-    //connectionID := "nqm-agent@10.20.30.40"
+var connectionId = flag.String("connectionId", "", "The id of connection")
 
-	var samplingTargetList []SamplingTarget    
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"203.208.150.145", "ChinaTelecom", "Zhejiang", "Hangzhou", "Room-1"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"203.208.146.33", "ChinaTelecom", "Zhejiang", "Hangzhou", "Room-1"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"203.208.232.53", "ChinaTelecom", "Zhejiang", "Hangzhou", "Room-2"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"211.160.177.145", "Chinanet", "Zhejiang", "Wenzhou", "Room-1"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"211.160.177.150", "Chinanet", "Zhejiang", "Wenzhou", "Room-2"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"211.160.177.70", "Chinanet", "Zhejiang", "Wenzhou", "Room-2"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"61.190.194.110", "ChinaTelecom", "Anhui", "Hefei", "Room-1"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"61.190.194.114", "ChinaTelecom", "Anhui", "Hefei", "Room-2"})
-    samplingTargetList = append(samplingTargetList, SamplingTarget{"61.190.194.222", "ChinaTelecom", "Anhui", "Hefei", "Room-2"})
+
+func main() {
+
+    flag.Parse()
+
+    urlPush := "http://10.20.30.40:1988/v1/push"
+
+    var rpcClient SingleConnRpcClient
+
+    rpcClient.RpcServer = "10.20.30.40:6030"
+
+    if *connectionId == "" {
+        *connectionId = "nqm-agent-1@10.20.30.40"
+    }
+
+    var req = model.NqmPingTaskRequest {
+        ConnectionId: *connectionId,
+        Hostname: "nqm-agent-1",
+        IpAddress: "10.20.30.40",
+    }
+    var resp model.NqmPingTaskResponse
+
+    fmt.Println("Request: ", req)
+
+    err := rpcClient.Call("NqmAgent.PingTask", req, &resp,)
+    if err != nil {
+        fmt.Printf("Call NqmAgent.PingTask error: %v", err)
+    }
+
+    fmt.Printf("Agent: %v", resp.Agent)
+
+
+    if !resp.NeedPing {
+        fmt.Println(resp.NeedPing)
+        return
+    }
+
+	fmt.Printf("resposne: %v", resp)
+    resp.Command[6]="100"
+    commandTemplate := resp.Command
+    fmt.Println(req)
+    fmt.Println("resp.Agent.Id=", resp.Agent.Id, "resp.Agent.IspId=", resp.Agent.IspId, "resp.Agent.ProvinceId=", resp.Agent.ProvinceId, "resp.Agent.CityId=", resp.Agent.CityId)
+    fmt.Println("resp.Agent.Name=", resp.Agent.Name, "resp.Agent.IspName=", resp.Agent.IspName, "resp.Agent.ProvinceName=", resp.Agent.ProvinceName, "resp.Agent.CityName=", resp.Agent.CityName)
+    fmt.Println(commandTemplate)
+    var samplingTargetList []SamplingTarget
+    for _, target := range resp.Targets {
+        fmt.Println( target.Host, target.IspName, target.ProvinceName, target.CityName, target.NameTag)
+        samplingTargetList = append(samplingTargetList, SamplingTarget{target.Host, target.IspName, target.ProvinceName, target.CityName, target.NameTag})
+    }
     
-    statisticsOfPacketsSent, statisticsOfPacketsReceived, statisticsOfTransmissionTime := Probe(samplingTargetList)
+    statisticsOfPacketsSent, statisticsOfPacketsReceived, statisticsOfTransmissionTime := Probe(samplingTargetList, commandTemplate)
     
     var params [] ParamToAgent
-    params = append(params, FormParams("packets-sent" ,statisticsOfPacketsSent)...)
-    params = append(params, FormParams("packets-received", statisticsOfPacketsReceived)...)
-    params = append(params, FormParams("transmission-time", statisticsOfTransmissionTime)...)
+    params = append(params, FormParams(resp.Agent, "packets-sent" ,statisticsOfPacketsSent)...)
+    params = append(params, FormParams(resp.Agent, "packets-received", statisticsOfPacketsReceived)...)
+    params = append(params, FormParams(resp.Agent, "transmission-time", statisticsOfTransmissionTime)...)
     
     
     paramsBody, err := json.Marshal(params)
@@ -84,18 +128,21 @@ func main() {
     fmt.Println("Body:", string(paramsBody))
 }
 
-func FormParams(metric string, statistics map[SamplingTarget]Statistic) []ParamToAgent {
+func FormParams(agent *model.NqmAgent, metric string, statistics map[SamplingTarget]Statistic) []ParamToAgent {
     var params [] ParamToAgent
     
     for samplingTarget, statistic := range statistics {
-        endpoint := "nqm-agent-1@10.20.30.40"
+        endpoint := *connectionId
         value := statistic.Value
         counterType := "GAUGE"
-        tags := "target="+samplingTarget.Address+
-                ",isp="+samplingTarget.ISP+
-                ",province="+samplingTarget.Province+
-                ",city="+samplingTarget.City+
-                ",name_tag="+samplingTarget.NameTag
+        tags := ",nqm-agent-isp="+agent.IspName+
+                ",nqm-agent-province="+agent.ProvinceName+
+                ",nqm-agent-city="+agent.CityName+
+                ",target-ip="+samplingTarget.Address+
+                ",target-isp="+samplingTarget.ISP+
+                ",target-province="+samplingTarget.Province+
+                ",target-city="+samplingTarget.City+
+                ",target-name-tag="+samplingTarget.NameTag
         timestamp := statistic.Timestamp
         step := int64(60)
         
@@ -105,7 +152,7 @@ func FormParams(metric string, statistics map[SamplingTarget]Statistic) []ParamT
     return params
 }
 
-func Probe(samplingTargetList []SamplingTarget) (map[SamplingTarget]Statistic, map[SamplingTarget]Statistic, map[SamplingTarget]Statistic) {
+func Probe(samplingTargetList []SamplingTarget, commandTemplate []string) (map[SamplingTarget]Statistic, map[SamplingTarget]Statistic, map[SamplingTarget]Statistic) {
     statisticsOfPacketsSent := make(map[SamplingTarget]Statistic)
     statisticsOfPacketsReceived := make(map[SamplingTarget]Statistic)
     statisticsOfTransmissionTime := make(map[SamplingTarget]Statistic)
@@ -113,9 +160,9 @@ func Probe(samplingTargetList []SamplingTarget) (map[SamplingTarget]Statistic, m
     for _, samplingTarget := range samplingTargetList {
         targetAddressList = append(targetAddressList, samplingTarget.Address)
     }
-    commandTemplate := []string{"-p", "20", "-i", "10", "-c", "100", "-q", "-a"}
+    //commandTemplate example: {"fping", "-p", "20", "-i", "10", "-c", "100", "-q", "-a"}
     commandTemplate = append(commandTemplate, targetAddressList...)
-    fpingCommand := exec.Command("fping", commandTemplate...)
+    fpingCommand := exec.Command(commandTemplate[0], commandTemplate[1:]...)
     cmdOutput, err := fpingCommand.CombinedOutput()
     if err != nil {
         fmt.Println("error occured:")
@@ -168,4 +215,78 @@ func Probe(samplingTargetList []SamplingTarget) (map[SamplingTarget]Statistic, m
     }
     
     return statisticsOfPacketsSent, statisticsOfPacketsReceived, statisticsOfTransmissionTime
+}
+
+
+type SingleConnRpcClient struct {
+	sync.Mutex
+	rpcClient *rpc.Client
+	RpcServer string
+	Timeout   time.Duration
+}
+
+func (this *SingleConnRpcClient) close() {
+	if this.rpcClient != nil {
+		this.rpcClient.Close()
+		this.rpcClient = nil
+	}
+}
+
+func (this *SingleConnRpcClient) insureConn() {
+	if this.rpcClient != nil {
+		return
+	}
+
+	var err error
+	var retry int = 1
+
+	for {
+		if this.rpcClient != nil {
+			return
+		}
+
+		this.rpcClient, err = net.JsonRpcClient("tcp", this.RpcServer, this.Timeout)
+		if err == nil {
+			return
+		}
+
+		log.Printf("dial %s fail: %v", this.RpcServer, err)
+
+		if retry > 6 {
+			retry = 1
+		}
+
+		time.Sleep(time.Duration(math.Pow(2.0, float64(retry))) * time.Second)
+
+		retry++
+	}
+}
+
+func (this *SingleConnRpcClient) Call(method string, args interface{}, reply interface{}) error {
+
+	this.Lock()
+	defer this.Unlock()
+
+	this.insureConn()
+
+	timeout := time.Duration(50 * time.Second)
+	done := make(chan error)
+
+	go func() {
+		err := this.rpcClient.Call(method, args, reply)
+		done <- err
+	}()
+
+	select {
+	case <-time.After(timeout):
+		log.Printf("[WARN] rpc call timeout %v => %v", this.rpcClient, this.RpcServer)
+		this.close()
+	case err := <-done:
+		if err != nil {
+			this.close()
+			return err
+		}
+	}
+
+	return nil
 }
