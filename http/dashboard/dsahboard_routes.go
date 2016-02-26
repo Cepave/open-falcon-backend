c12f443e4eab97b6ab26cb60484609540e3a9db6
masato25 <leomasato25@gmail.com>
2016-02-27 01:47:01 +0800

    fixed routes typo

--- http/dashboard/dsahboard_routes.go
+++ http/dashboard/dsahboard_routes.go
@@ -11,8 +11,8 @@ func ConfigRoutes() {
 		beego.NSGet("/notallowed", func(ctx *context.Context) {
 			ctx.Output.Body([]byte("notAllowed"))
 		}),
-		beego.NSRouter("/endpoints", &BashBoardController{}, "get:EndpRegxqury,post:EndpRegxqury"),
-		beego.NSRouter("/endpointcounters", &BashBoardController{}, "get:CounterQuery,post:CounterQuery"),
+		beego.NSRouter("/endpoints", &BashBoardController{}, "get:EndpRegxqury;post:EndpRegxqury"),
+		beego.NSRouter("/endpointcounters", &BashBoardController{}, "get:CounterQuery;post:CounterQuery"),
 	)
 	beego.AddNamespace(dashboard)
 }
