fbd22f3071bc75472f394c58647550c8d07e1340
Don <don@cepave.com>
2016-03-09 15:57:31 +0800

    [OWL-302] add CREATE TABLE tags in db_schema/portal-db-schema.sql

--- db_schema/portal-db-schema.sql
+++ db_schema/portal-db-schema.sql
@@ -166,6 +166,19 @@ CREATE TABLE `action` (
   COLLATE =utf8_unicode_ci;
 
 /**
+ * tags table for setting template variables
+ */
+DROP TABLE IF EXISTS tags;
+CREATE TABLE `tags` (
+  `strategy_id` INT(10) UNSIGNED NOT NULL,
+  `name`     VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'tag name',
+  `value`     VARCHAR(32) NOT NULL DEFAULT '' COMMENT 'tag value',
+  `create_at` DATETIME NOT NULL COMMENT 'create time',
+  `update_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'last modify time',
+  PRIMARY KEY (`strategy_id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/**
  * nodata mock config
  */
 DROP TABLE IF EXISTS `mockcfg`;
