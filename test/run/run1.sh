d6a8305b18182132f357f882f69690085ff06a55
chyeh <chyeh@cepave.com>
2016-04-29 10:02:17 +0800

    Delete `test/`
    These SQL and shell scripts are no longer needed.

--- test/run/run1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while :
-do
-	../../nqm 1>/dev/null
-	sleep 30;
-done
--- test/run/run2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while :
-do
-	../../nqm -connectionId nqm-agent-2@10.20.30.40 1>/dev/null
-	sleep 30;
-done
