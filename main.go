4f914eb8c3c7fa6fc21ac48b9f6ec9ee828b0583
chyeh <chyeh@cepave.com>
2016-05-23 13:00:32 +0800

    [OWL-611] Add functions for probing utilities
    NQM agent will support 3 kinds of probes: `fping`, `tcpping`, and
    `tcpconn`. They will be executed in corresponding goroutines. In this
    commit, the functions are just added and not implemented.

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 )
 
 func init() {
@@ -26,26 +25,7 @@ func main() {
 	InitRPC()
 
 	go QueryHbs()
+	Measure()
 
-	go func() {
-		for {
-			func() {
-				probingCmd, targets, agentPtr, err := makeTasks()
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				probingCmd = []string{"fping", "-p", "20", "-i", "10", "-C", "4", "-q", "-a", "127.0.0.1"}
-				log.Println("Execution the probing command:", probingCmd[0:9])
-
-				rawData := Probe(probingCmd)
-				jsonParams := MarshalIntoParameters(rawData, targets, agentPtr)
-				Push(jsonParams)
-			}()
-
-			dur := time.Second * time.Duration(GetGeneralConfig().Agent.FpingInterval)
-			time.Sleep(dur)
-		}
-	}()
 	select {}
 }
---
+++ measure.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+func tcpconn() {
+	for {
+		log.Println("Execution of tcpcpnn")
+
+		dur := time.Second * time.Duration(GetGeneralConfig().Agent.TcpconnInterval)
+		time.Sleep(dur)
+	}
+}
+
+func tcpping() {
+	for {
+		log.Println("Execution of tcpping")
+
+		dur := time.Second * time.Duration(GetGeneralConfig().Agent.TcppingInterval)
+		time.Sleep(dur)
+	}
+}
+
+func fping() {
+	for {
+		func() {
+			probingCmd, targets, agentPtr, err := makeTasks()
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			probingCmd = []string{"fping", "-p", "20", "-i", "10", "-C", "4", "-q", "-a", "127.0.0.1"}
+			log.Println("Execution of the probing command:", probingCmd[0:9])
+
+			rawData := Probe(probingCmd)
+			jsonParams := MarshalIntoParameters(rawData, targets, agentPtr)
+			Push(jsonParams)
+		}()
+
+		dur := time.Second * time.Duration(GetGeneralConfig().Agent.FpingInterval)
+		time.Sleep(dur)
+	}
+}
+
+func Measure() {
+	go fping()
+	go tcpping()
+	go tcpconn()
+}
