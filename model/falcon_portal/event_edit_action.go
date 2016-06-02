635394e34bdb3ef12c7ce611dc5aa64de4325f91
masato25 <leomasato25@gmail.com>
2016-06-02 20:06:38 +0800

    modified code for saving process info into event_cases

--- model/falcon_portal/event_edit_action.go
+++ model/falcon_portal/event_edit_action.go
@@ -16,24 +16,33 @@ func CloseEvent(username string, colsed_note string, id string) (err error) {
 	return
 }
 
+func UpdateProcess(processNoteID int, processStatus string, id string) (err error) {
+	q := orm.NewOrm()
+	q.Using("falcon_portal")
+	_, err = q.Raw("Update event_cases SET process_note = ?, process_status = ? WHERE id = ?", processNoteID, processStatus, id).Exec()
+	return
+}
+
 func AddNote(username string, note string, id string, status string, caseId string) (err error) {
 	q := orm.NewOrm()
 	q.Using("falcon_portal")
 	userid := uic.ReadUserIdByName(username)
-	sqlbase := fmt.Sprintf("SET event_caseId = '%s' , user_id = %d", id, userid)
+	sqlbase := fmt.Sprintf("SET event_caseId = %s , user_id = %d, timestamp = %v", id, userid, time.Now())
 	if note != "" {
 		sqlbase = fmt.Sprintf("%s , note = '%s'", sqlbase, note)
 	}
 	if status != "" {
 		sqlbase = fmt.Sprintf("%s, status = '%s'", sqlbase, status)
-		if status == "Sovled" {
-			CloseEvent(username, note, id)
-		}
 	}
 	//for set boss case id
 	if caseId != "" {
 		sqlbase = fmt.Sprintf("%s, case_id = '%s'", sqlbase, caseId)
 	}
-	_, err = q.Raw(fmt.Sprintf("Insert INTO event_note %s, timestamp = ?", sqlbase), time.Now()).Exec()
+	var eventNote EventNote
+	q.Raw(fmt.Sprintf("Insert INTO event_note %s", sqlbase)).QueryRow(&eventNote)
+
+	if eventNote.Status == "Sovled" || eventNote.Status == "In Process" {
+		err = UpdateProcess(eventNote.Id, eventNote.Status, id)
+	}
 	return
 }
