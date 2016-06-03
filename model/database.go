588ee98f05eabba6452b1a80daf51ceee43804ff
masato25 <leomasato25@gmail.com>
2016-06-03 16:38:12 +0800

    reformat orm registration

--- model/database.go
+++ model/database.go
@@ -10,16 +10,17 @@ import (
 )
 
 func InitDatabase() {
-	// set default database
+
+	// register model
+	orm.RegisterModel(new(uic.User), new(uic.Team), new(uic.Session),
+		new(uic.RelTeamUser), new(dashboard.Endpoint), new(dashboard.EndpointCounter),
+		new(dashboard.HostGroup), new(dashboard.Hosts), new(event.EventCases),
+		new(event.Events), new(event.Tpl), new(event.EventNote))
 	// databases_name, sql_derver, db_addr, idle_time, mix_connection
 	config := g.Config()
 	orm.RegisterDataBase("default", "mysql", config.Uic.Addr, config.Uic.Idle, config.Uic.Max)
 	orm.RegisterDataBase("graph", "mysql", config.GraphDB.Addr, config.GraphDB.Idle, config.GraphDB.Max)
 	orm.RegisterDataBase("falcon_portal", "mysql", config.FalconPortal.Addr, config.FalconPortal.Idle, config.FalconPortal.Max)
-
-	// register model
-	orm.RegisterModel(new(uic.User), new(uic.Team), new(uic.Session), new(uic.RelTeamUser), new(dashboard.Endpoint),
-		new(dashboard.EndpointCounter), new(dashboard.HostGroup), new(dashboard.Hosts), new(event.EventCases), new(event.Events), new(event.Tpl))
 
 	if config.Log == "debug" {
 		orm.Debug = true
