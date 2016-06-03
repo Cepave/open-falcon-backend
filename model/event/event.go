f30906ed67bfd7f525d8fa7689bec5c5876ce98c
masato25 <leomasato25@gmail.com>
2016-06-03 12:29:39 +0800

    update event case model && event case update methods

--- model/event/event.go
+++ model/event/event.go
@@ -13,20 +13,19 @@ type EventCases struct {
 	Cond     string `json:"cond"`
 	Note     string `json:"note"`
 	//leftValue + operator + rightValue
-	MaxStep      int       `json:"max_step"`
-	CurrentStep  int       `json:"current_step"`
-	Priority     int       `json:"priority"`
-	Status       string    `json:"status"`
-	Timestamp    time.Time `json:"start_at"`
-	UpdateAt     time.Time `json:"update_at"`
-	ClosedAt     time.Time `json:"closed_at"`
-	ClosedNote   string    `json:"closed_note"`
-	UserModified int       `json:"user_modified"`
-	TplCreator   string    `json:"tpl_creator"`
-	ExpressionId int       `json:"expression_id"`
-	StrategyId   int       `json:"strategy_id"`
-	TemplateId   int       `json:"template_id"`
-	Events       []*Events `json:"evevnts" orm:"reverse(many)"`
+	MaxStep       int       `json:"max_step"`
+	CurrentStep   int       `json:"current_step"`
+	Priority      int       `json:"priority"`
+	Status        string    `json:"status"`
+	Timestamp     time.Time `json:"start_at"`
+	UpdateAt      time.Time `json:"update_at"`
+	ProcessNote   int       `json:"process_note"`
+	ProcessStatus string    `json:"process_status"`
+	TplCreator    string    `json:"tpl_creator"`
+	ExpressionId  int       `json:"expression_id"`
+	StrategyId    int       `json:"strategy_id"`
+	TemplateId    int       `json:"template_id"`
+	Events        []*Events `json:"evevnts" orm:"reverse(many)"`
 }
 
 type Events struct {
