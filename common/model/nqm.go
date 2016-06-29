614243b19b9a64ce6b7066ac1a0e81991f3a9001
Kordan Ou <kordan@hitripod.com>
2016-06-29 16:25:19 +0800

    removed submodule common

---
+++ common/model/nqm.go
@@ -0,0 +1,127 @@
+package model
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	// Value of undefined id
+	UNDEFINED_ID = -1
+
+	UNDEFINED_ISP_ID      = int16(UNDEFINED_ID)
+	UNDEFINED_PROVINCE_ID = int16(UNDEFINED_ID)
+	UNDEFINED_CITY_ID     = int16(UNDEFINED_ID)
+
+	// Value of undefined string
+	UNDEFINED_STRING = "<UNDEFINED>"
+)
+
+// NqmTaskRequest represents the request for ping task by NQM agent
+type NqmTaskRequest struct {
+	// The connection id of agent(used to identify task configruation)
+	ConnectionId string `valid:"required"`
+	// The hostname of agent
+	Hostname string `valid:"required"`
+	// The IP address of agent
+	// Could be IPv4 or IPv6 format
+	IpAddress string `valid:"required"`
+}
+
+type MeasurementsProperty struct {
+	Enabled  bool
+	Command  []string
+	Interval time.Duration
+}
+
+// NqmTaskResponse represents the response for ping task requested from NQM agent
+//
+// If NeedPing is false, Targets and Command would be empty array
+type NqmTaskResponse struct {
+	// Whether or not the task should be performed
+	NeedPing bool
+
+	// The data of agent
+	// nil if there is no need for ping
+	Agent *NqmAgent
+
+	// The list of target hosts to be probed(ping)
+	// nil if there is no need for ping
+	Targets []NqmTarget
+
+	// The command/arguments of command to be executed
+	// nil if there is no need for ping
+	Measurements map[string]MeasurementsProperty
+}
+
+// Represents the data of agent
+type NqmAgent struct {
+	// The id of agent
+	Id int
+
+	// The name of agent
+	Name string
+
+	// The id of ISP, UNDEFINED_ID means there is not such data for this target
+	IspId int16
+	// The name of ISP
+	IspName string
+
+	// The id of province, UNDEFINED_ID means there is not such data for this target
+	ProvinceId int16
+	// The name of province
+	ProvinceName string
+
+	// The id of city, UNDEFINED_ID means there is not such data for this target
+	CityId int16
+	// The name of city
+	CityName string
+}
+
+// Represents the data of target used by NQM agent
+type NqmTarget struct {
+	// The id of target
+	Id int
+
+	// The IP address or FQDN used by ping command
+	Host string
+
+	// The id of ISP, UNDEFINED_ID means there is not such data for this target
+	IspId int16
+	// The name of ISP
+	IspName string
+
+	// The id of province, UNDEFINED_ID means there is not such data for this target
+	ProvinceId int16
+	// The name of province
+	ProvinceName string
+
+	// The id of city, UNDEFINED_ID means there is not such data for this target
+	CityId int16
+	// The name of city
+	CityName string
+
+	// The tag of the target, UNDEFINED_STRING means no such data for this target
+	NameTag string
+}
+
+func (target NqmTarget) String() string {
+	return fmt.Sprintf(
+		"Id: [%d] Host: [%s] Isp: \"%s\"(%d) Province: \"%s\"(%d), City: \"%s\"[%d], Name tag: [%s]",
+		target.Id, target.Host,
+		target.IspName, target.IspId,
+		target.ProvinceName, target.ProvinceId,
+		target.CityName, target.CityId,
+		target.NameTag,
+	)
+}
+
+func (agent NqmAgent) String() string {
+	return fmt.Sprintf(
+		"Id: [%d] Isp: \"%s\"(%d) Province: \"%s\"(%d), City: \"%s\"[%d]",
+		agent.Id,
+		agent.IspName, agent.IspId,
+		agent.ProvinceName, agent.ProvinceId,
+		agent.CityName, agent.CityId,
+	)
+}
---
+++ common/net/ip.go
@@ -0,0 +1,63 @@
+package net
+
+import (
+	"net"
+	"strings"
+
+	n "github.com/toolkits/net"
+)
+
+func InternetIP() (ips []string, err error) {
+	ips = make([]string, 0)
+
+	ifaces, e := net.Interfaces()
+	if e != nil {
+		return ips, e
+	}
+
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+
+		// ignore docker and warden bridge
+		if strings.HasPrefix(iface.Name, "docker") || strings.HasPrefix(iface.Name, "w-") {
+			continue
+		}
+
+		addrs, e := iface.Addrs()
+		if e != nil {
+			return ips, e
+		}
+
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			if ip == nil || ip.IsLoopback() {
+				continue
+			}
+
+			ip = ip.To4()
+			if ip == nil {
+				continue // not an ipv4 address
+			}
+
+			ipStr := ip.String()
+			if !n.IsIntranet(ipStr) {
+				ips = append(ips, ipStr)
+			}
+		}
+	}
+
+	return ips, nil
+}
