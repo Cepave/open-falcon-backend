ab9433b258683edad742a493d821afe92240023d
Minimum <s5550055@yahoo.com.tw>
2016-07-05 15:54:35 +0800

    Remove unavailable git commit message

--- modules/query/main.go
+++ modules/query/main.go
@@ -19,15 +19,10 @@ import (
 func main() {
 	cfg := flag.String("c", "cfg.json", "specify config file")
 	version := flag.Bool("v", false, "show version")
-	versionGit := flag.Bool("vg", false, "show version and git commit log")
 	flag.Parse()
 
 	if *version {
 		fmt.Println(g.VERSION)
-		os.Exit(0)
-	}
-	if *versionGit {
-		fmt.Println(g.VERSION, g.COMMIT)
 		os.Exit(0)
 	}
 
