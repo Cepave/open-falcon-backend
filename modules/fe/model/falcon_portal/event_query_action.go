1da33a605b12fc14b617f9203efc04f5780e0e72
masato25 <help0999@gmail.com>
2016-07-07 11:54:36 +0800

    refactor portal api code and update api doc

--- modules/fe/model/falcon_portal/event_query_action.go
+++ modules/fe/model/falcon_portal/event_query_action.go
@@ -7,30 +7,37 @@ import (
 
 	"github.com/Cepave/open-falcon-backend/modules/fe/g"
 	"github.com/Cepave/open-falcon-backend/modules/fe/model/uic"
+	log "github.com/Sirupsen/logrus"
 	"github.com/astaxie/beego/orm"
-	"time"
 )
 
 //generate status filter SQL templete
-func genStatusQueryTemplete(status string, feildsName string, flag bool) (filterTemplete string, vflag bool) {
-	if flag {
-		filterTemplete = fmt.Sprintf(" AND %v ", filterTemplete)
+func genStatusQueryTemplete(status string, feildsName string) (filterTemplete string) {
+	statusList := strings.Split(status, ",")
+	var filterTempleteArr []string
+	for _, s := range statusList {
+		filterTempleteArr = append(filterTempleteArr, fmt.Sprintf("%s = '%s'", feildsName, s))
 	}
-	vflag = true
-	filterTemplete = fmt.Sprintf("%v (", filterTemplete)
-	status_list := strings.Split(status, ",")
-	for idx, s := range status_list {
-		if idx == len(status_list)-1 {
-			filterTemplete = fmt.Sprintf("%v %s = '%s')", filterTemplete, feildsName, s)
-		} else {
-			filterTemplete = fmt.Sprintf("%v %s = '%s' or ", filterTemplete, feildsName, s)
-		}
-	}
-	return filterTemplete, vflag
+	filterTemplete = strings.Join(filterTempleteArr, " OR ")
+	filterTemplete = fmt.Sprintf("( %s )", filterTemplete)
+	return
 }
 
-func GetEventCases(startTime int64, endTime int64, priority int, status string, progressStatus string, limit int, elimit int, username string, metrics string) (result []EventCases, err error) {
+func genSqlFilterTemplete(whereConditions []string) string {
+	if len(whereConditions) == 0 {
+		return ""
+	}
+	conditions := strings.Join(whereConditions, " AND ")
+	return fmt.Sprintf("WHERE %s", conditions)
+}
+
+const SkipFilter = "ALL"
+
+func GetEventCases(startTime int64, endTime int64, priority int, status string, progressStatus string, limit int, elimit int, username string, metrics string, caseId string) (result []EventCases, err error) {
 	config := g.Config()
+	q := orm.NewOrm()
+	q.Using("falcon_portal")
+	var whereConditions []string
 	if limit == 0 || limit > config.FalconPortal.Limit {
 		limit = config.FalconPortal.Limit
 	}
@@ -39,110 +46,92 @@ func GetEventCases(startTime int64, endTime int64, priority int, status string, 
 	if tplids == "" {
 		tplids = "-1"
 	}
-	q := orm.NewOrm()
-	q.Using("falcon_portal")
-	flag := false
-	queryTmp := ""
+
+	//fot generate sql filter
 	if startTime != 0 && endTime != 0 {
-		flag = true
-		stime := time.Unix(startTime, 0).Format("2016-06-23 00:00:00")
-		etime := time.Unix(endTime, 0).Format("2016-06-23 00:00:00")
-		queryTmp = fmt.Sprintf(" %v update_at >= '%s' AND  update_at <= '%s'", queryTmp, stime, etime)
+		whereConditions = append(whereConditions, fmt.Sprintf("update_at BETWEEN FROM_UNIXTIME(%d) AND FROM_UNIXTIME(%d)", startTime, endTime))
 	}
 	if priority != -1 {
-		if flag {
-			queryTmp = fmt.Sprintf("%v and priority = %d", queryTmp, priority)
-		} else {
-			flag = true
-			queryTmp = fmt.Sprintf("%v priority = %d", queryTmp, priority)
-		}
+		whereConditions = append(whereConditions, fmt.Sprintf("priority = %d", priority))
 	}
-	if status != "ALL" {
-		var statusFilterTmp string
-		statusFilterTmp, flag = genStatusQueryTemplete(status, "status", flag)
-		queryTmp = fmt.Sprintf("%v %s", queryTmp, statusFilterTmp)
+	if status != SkipFilter {
+		log.Debug("statis ", status)
+		whereConditions = append(whereConditions, genStatusQueryTemplete(status, "status"))
 	}
-	if progressStatus != "ALL" {
-		var progressFilterTmp string
-		progressFilterTmp, flag = genStatusQueryTemplete(progressStatus, "process_status", flag)
-		queryTmp = fmt.Sprintf("%v %s", queryTmp, progressFilterTmp)
+	if progressStatus != SkipFilter {
+		whereConditions = append(whereConditions, genStatusQueryTemplete(progressStatus, "process_status"))
 	}
-
-	if metrics != "ALL" {
-		var metricFilterTmp string
-		metricFilterTmp, flag = genStatusQueryTemplete(metrics, "metric", flag)
-		queryTmp = fmt.Sprintf("%v %s", queryTmp, metricFilterTmp)
+	if metrics != SkipFilter {
+		whereConditions = append(whereConditions, genStatusQueryTemplete(metrics, "metric"))
 	}
-
-	if queryTmp != "" && !isadmin {
-		_, err = q.Raw(fmt.Sprintf("SELECT * FROM `event_cases` WHERE (tpl_creator = '%s' OR template_id in (%s)) AND %v order by update_at DESC limit %d", username, tplids, queryTmp, limit)).QueryRows(&result)
-	} else {
-		if queryTmp != "" {
-			queryTmp = fmt.Sprintf("WHERE %v", queryTmp)
-		}
-		_, err = q.Raw(fmt.Sprintf("SELECT * FROM `event_cases` %v order by update_at DESC limit %d", queryTmp, limit)).QueryRows(&result)
+	if caseId != "" {
+		whereConditions = append(whereConditions, fmt.Sprintf("id = '%s'", caseId))
 	}
+	//perpare ssql statement
+	if !isadmin {
+		whereConditions = append(whereConditions, fmt.Sprintf("(tpl_creator = '%s' OR template_id in (%s))", username, tplids))
+	}
+	_, err = q.Raw(fmt.Sprintf("SELECT * FROM `event_cases` %s limit %d", genSqlFilterTemplete(whereConditions), limit)).QueryRows(&result)
 
 	if len(result) == 0 {
 		result = []EventCases{}
-	} else {
-		var eventLimit int
-		if eventLimit = elimit; elimit == 0 {
-			eventLimit = 10
+		return
+	}
+	//set default number of event
+	var eventLimit int
+	if eventLimit = elimit; elimit == 0 {
+		eventLimit = 10
+	}
+	for indx, event := range result {
+		var eventArr []*Events
+		q.Raw(fmt.Sprintf("SELECT * FROM `events` WHERE event_caseId = '%s' order by timestamp DESC Limit %d", event.Id, eventLimit)).QueryRows(&eventArr)
+		if len(eventArr) != 0 {
+			event.Events = eventArr
+		} else {
+			event.Events = []*Events{}
 		}
-		for indx, event := range result {
-			var eventArr []*Events
-			q.Raw(fmt.Sprintf("SELECT * FROM `events` WHERE event_caseId = '%s' order by timestamp DESC Limit %d", event.Id, eventLimit)).QueryRows(&eventArr)
-			if len(eventArr) != 0 {
-				event.Events = eventArr
-			} else {
-				event.Events = []*Events{}
-			}
-			result[indx] = event
-		}
+		result[indx] = event
 	}
 	return
 }
 
-func GetEvents(startTime int64, endTime int64, status string, limit int) (result []EventsRsp, err error) {
+func GetEvents(startTime int64, endTime int64, status string, limit int, caseId string) (result []EventsRsp, err error) {
+
 	config := g.Config()
-	if limit == 0 || limit > config.FalconPortal.Limit {
+	q := orm.NewOrm()
+	q.Using("falcon_portal")
+
+	var whereConditions []string
+
+	if status != SkipFilter {
+		if status == "OK" {
+			whereConditions = append(whereConditions, fmt.Sprintf("events.status = %d", 1))
+		} else if status == "PROBLEM" {
+			whereConditions = append(whereConditions, fmt.Sprintf("events.status = %d", 0))
+		}
+	}
+
+	if startTime != 0 && endTime != 0 {
+		whereConditions = append(whereConditions, fmt.Sprintf("events.timestamp BETWEEN FROM_UNIXTIME(%d) AND FROM_UNIXTIME(%d)", startTime, endTime))
+	}
+	if caseId != "" {
+		whereConditions = append(whereConditions, fmt.Sprintf("events.event_caseId = '%s'", caseId))
+	}
+	if limit == 0 {
 		limit = config.FalconPortal.Limit
 	}
 
-	q := orm.NewOrm()
-	q.Using("falcon_portal")
-	queryTmp := ""
-	if status != "ALL" {
-		queryTmp = fmt.Sprintf(" %v status = '%s' and ", queryTmp, status)
-	}
-	if startTime != 0 && endTime != 0 {
-		queryTmp = fmt.Sprintf(" %v events.timestamp >= %d and events.timestamp <= %d", queryTmp, startTime, endTime)
-	}
-	if queryTmp != "" {
-		_, err = q.Raw(fmt.Sprintf(`SELECT events.id as id,
-					events.step as step,
-					events.cond as cond,
-					events.timestamp as timestamp,
-					events.event_caseId as eid,
-					event_cases.tpl_creator as tpl_creator,
-					event_cases.metric as metric,
-					event_cases.endpoint as endpoint
-					FROM events LEFT JOIN event_cases on event_cases.id = events.event_caseId
-					WHERE %v ORDER BY events.timestamp DESC limit %d`, queryTmp, limit)).QueryRows(&result)
-	} else {
-		_, err = q.Raw(fmt.Sprintf(`SELECT
-					events.id as id,
-					events.step as step,
-					events.cond as cond,
-					events.timestamp as timestamp,
-					events.event_caseId as eid,
-					event_cases.tpl_creator as tpl_creator,
-					event_cases.metric as metric,
-					event_cases.endpoint as endpoint
-					FROM events LEFT JOIN event_cases on event_cases.id = events.event_caseId
-					ORDER BY events.timestamp DESC limit %d`, limit)).QueryRows(&result)
-	}
+	_, err = q.Raw(fmt.Sprintf(`SELECT events.id as id,
+				events.step as step,
+				events.cond as cond,
+				events.timestamp as timestamp,
+				events.event_caseId as eid,
+				event_cases.tpl_creator as tpl_creator,
+				event_cases.metric as metric,
+				event_cases.endpoint as endpoint
+				FROM events LEFT JOIN event_cases on event_cases.id = events.event_caseId
+				%s ORDER BY events.timestamp DESC limit %d`, genSqlFilterTemplete(whereConditions), limit)).QueryRows(&result)
+
 	if len(result) == 0 {
 		result = []EventsRsp{}
 	}
@@ -158,22 +147,30 @@ func CountNumOfTlp() (c int, err error) {
 	return
 }
 
-func GetNotes(event_caseId string, limit int) (enotes []EventNote, err error) {
+func GetNotes(eventCaseId string, limit int, startTime int64, endTime int64, filterIgnored bool) (enotes []EventNote, err error) {
+	q := orm.NewOrm()
+	q.Using("falcon_portal")
+	whereConditions := []string{fmt.Sprintf("event_note.event_caseId = '%s' ", eventCaseId)}
+	if startTime > 0 && endTime > 0 {
+		whereConditions = append(whereConditions, fmt.Sprintf("event_note.timestamp BETWEEN FROM_UNIXTIME(%d) AND FROM_UNIXTIME(%d)", startTime, endTime))
+	}
+	if filterIgnored {
+		whereConditions = append(whereConditions, "event_note.status != 'ignored' ")
+	}
 	if limit == 0 {
 		limit = 10
 	}
-	q := orm.NewOrm()
-	q.Using("falcon_portal")
-	_, err = q.Raw(fmt.Sprintf(`SELECT event_note.id as id,
-				event_note.event_caseId as event_caseId,
-				event_note.note as note,
-				event_note.case_id as case_id,
-				event_note.event_caseId as eid,
-				event_note.status as status,
-				event_note.timestamp as timestamp,
-				user.name as user_name
-				FROM falcon_portal.event_note as event_note LEFT JOIN uic.user as user on event_note.user_id = user.id
-				WHERE event_note.event_caseId = '%s' ORDER BY event_note.timestamp DESC limit %d`, event_caseId, limit)).QueryRows(&enotes)
+	sqlTemplete := fmt.Sprintf(`SELECT event_note.id as id,
+		event_note.event_caseId as event_caseId,
+		event_note.note as note,
+		event_note.case_id as case_id,
+		event_note.event_caseId as eid,
+		event_note.status as status,
+		event_note.timestamp as timestamp,
+		user.name as user_name
+		FROM falcon_portal.event_note as event_note LEFT JOIN uic.user as user on event_note.user_id = user.id
+		%s ORDER BY event_note.timestamp DESC limit %d`, genSqlFilterTemplete(whereConditions), limit)
+	_, err = q.Raw(sqlTemplete).QueryRows(&enotes)
 	if len(enotes) == 0 {
 		enotes = []EventNote{}
 	}
