fddac50f87ba82bb2179bced9032178d23c8eb52
Laurence Chen <humorless@gmail.com>
2016-07-26 03:44:52 +0000

    [OWL-693][agent] change the behavior the plugin/update API
    plugin/update API will remove the plugin dir and reclone again
    when it encounters 'git pull error'

--- modules/agent/http/plugin.go
+++ modules/agent/http/plugin.go
@@ -2,12 +2,35 @@ package http
 
 import (
 	"fmt"
+	"net/http"
+	"os/exec"
+
 	"github.com/Cepave/open-falcon-backend/modules/agent/g"
 	"github.com/Cepave/open-falcon-backend/modules/agent/plugins"
 	"github.com/toolkits/file"
-	"net/http"
-	"os/exec"
 )
+
+func deleteAndCloneRepo(w http.ResponseWriter) {
+	dir := g.Config().Plugin.Dir
+	parentDir := file.Dir(dir)
+	cmd1 := exec.Command("rm", "-rf", file.Basename(dir))
+	cmd1.Dir = parentDir
+	err1 := cmd1.Run()
+	if err1 != nil {
+		w.Write([]byte(fmt.Sprintf("\nremove the git plugin dir:%s fail. error: %s", file.Basename(dir), err1)))
+		return
+	} else {
+		w.Write([]byte("\nremove the git plugin dir success"))
+	}
+	cmd2 := exec.Command("git", "clone", g.Config().Plugin.Git, file.Basename(dir))
+	cmd2.Dir = parentDir
+	err2 := cmd2.Run()
+	if err2 != nil {
+		w.Write([]byte(fmt.Sprintf("\ngit clone in dir:%s fail. error: %s", parentDir, err2)))
+		return
+	}
+	w.Write([]byte("\ngit clone success"))
+}
 
 func configPluginRoutes() {
 	http.HandleFunc("/plugin/update", func(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +50,7 @@ func configPluginRoutes() {
 			err := cmd.Run()
 			if err != nil {
 				w.Write([]byte(fmt.Sprintf("git pull in dir:%s fail. error: %s", dir, err)))
+				deleteAndCloneRepo(w)
 				return
 			}
 		} else {
