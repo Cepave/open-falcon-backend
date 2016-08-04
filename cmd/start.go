1f46459d25d659cdaca714c5c6112d96d0fcbef2
chyeh <chyeh@cepave.com>
2016-08-04 17:04:33 +0800

    [OWL-912][backend] Introduction of `cobra`
    We replaced `mitchellh/cli` with `spf13/cobra`. The relative files of
    the implementations of commands are now under the `cmd` directory.

--- cmd/start.go
+++ cmd/start.go
@@ -1,4 +1,4 @@
-package start
+package cmd
 
 import (
 	"fmt"
@@ -8,23 +8,25 @@ import (
 	"time"
 
 	"github.com/Cepave/open-falcon-backend/g"
-	"github.com/mitchellh/cli"
+	"github.com/spf13/cobra"
 )
 
-// Command is a Command implementation that runs a Consul agent.
-// The command will not end unless a shutdown message is sent on the
-// ShutdownCh. If two messages are sent on the ShutdownCh it will forcibly
-// exit.
-type Command struct {
-	Revision          string
-	Version           string
-	VersionPrerelease string
-	Ui                cli.Ui
+var Start = &cobra.Command{
+	Use:   "start [Module ...]",
+	Short: "Start Open-Falcon modules",
+	Long: `
+Start the specified Open-Falcon modules and run until a stop command is received.
+A module represents a single node in a cluster.
+
+Modules:
+
+	` + "all " + strings.Join(g.AllModulesInOrder, " "),
+	RunE: start,
 }
 
-func (c *Command) Run(args []string) int {
+func start(c *cobra.Command, args []string) error {
 	if len(args) == 0 {
-		return cli.RunResultHelp
+		return c.Usage()
 	}
 	if (len(args) == 1) && (args[0] == "all") {
 		args = g.GetModuleArgsInOrder(g.AllModulesInOrder)
@@ -34,7 +36,7 @@ func (c *Command) Run(args []string) int {
 			if err != nil {
 				fmt.Println(err)
 				fmt.Println("** start failed **")
-				return g.Command_EX_ERR
+				return nil //g.Command_EX_ERR
 			}
 		}
 		args = g.GetModuleArgsInOrder(args)
@@ -47,14 +49,14 @@ func (c *Command) Run(args []string) int {
 			cmdArgs, err := g.GetConfFileArgs(g.ModuleConfs[moduleName])
 			if err != nil {
 				fmt.Println(err)
-				return g.Command_EX_ERR
+				return nil //g.Command_EX_ERR
 			}
 
 			logPath := "./" + moduleName + "/" + g.LogDir + "/" + moduleName + ".log"
 			LogOutput, err := os.OpenFile(logPath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
 			if err != nil {
 				fmt.Println("Error in opening file:", err)
-				return g.Command_EX_ERR
+				return nil //g.Command_EX_ERR
 			}
 			defer LogOutput.Close()
 
@@ -69,27 +71,10 @@ func (c *Command) Run(args []string) int {
 			moduleStatus = g.CheckModuleStatus(moduleName)
 			if moduleStatus == g.ModuleExistentNotRunning {
 				fmt.Println("** start failed **")
-				return g.Command_EX_ERR
+				return nil //g.Command_EX_ERR
 			}
 		}
 		// Skip starting if the module is already running
 	}
-	return g.Command_EX_OK
-}
-
-func (c *Command) Synopsis() string {
-	return "Start Open-Falcon modules"
-}
-
-func (c *Command) Help() string {
-	helpText := `
-Usage: open-falcon start [Module ...]
-
-  Start the specified Open-Falcon modules and run until a stop command is received.
-  A module represents a single node in a cluster.
-
-Modules:
-
-  ` + "all " + strings.Join(g.AllModulesInOrder, " ")
-	return strings.TrimSpace(helpText)
+	return nil //g.Command_EX_OK
 }
---
+++ cmd/status.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Cepave/open-falcon-backend/g"
+	"github.com/spf13/cobra"
+)
+
+var Status = &cobra.Command{
+	Use:   "status [Module ...]",
+	Short: "Check the status of Open-Falcon modules",
+	Long: `
+Check if the specified Open-Falcon modules are running, not running or noexistent.
+
+Modules:
+
+  ` + "all " + strings.Join(g.AllModulesInOrder, " "),
+	RunE: status,
+}
+
+func status(c *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return c.Usage()
+	}
+	if (len(args) == 1) && (args[0] == "all") {
+		args = g.AllModulesInOrder
+	} else {
+		for _, moduleName := range args {
+			err := g.ModuleExists(moduleName)
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("** status failed **")
+				return nil //g.Command_EX_ERR
+			}
+		}
+	}
+	for _, moduleName := range args {
+		g.CheckModuleStatus(moduleName)
+	}
+	return nil //g.Command_EX_OK
+}
