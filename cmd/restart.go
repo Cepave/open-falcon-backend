01c66fdc671524fcae641afc78d22e80bc47c165
chyeh <chyeh@cepave.com>
2016-08-04 17:04:33 +0800

    [OWL-912][backend] Introduction of `cobra`
    We replaced `mitchellh/cli` with `spf13/cobra`. The relative files of
    the implementations of commands are now under the `cmd` directory.

---
+++ cmd/restart.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Cepave/open-falcon-backend/g"
+	"github.com/spf13/cobra"
+)
+
+var Restart = &cobra.Command{
+	Use:   "restart [Module ...]",
+	Short: "Restart Open-Falcon modules",
+	Long: `
+Restart the specified Open-Falcon modules and run until a stop command is received.
+A module represents a single node in a cluster.
+
+Modules:
+
+  ` + "all " + strings.Join(g.AllModulesInOrder, " "),
+	RunE: restart,
+}
+
+func restart(c *cobra.Command, args []string) error {
+	if len(args) == 0 {
+		return c.Usage()
+	}
+	if (len(args) == 1) && (args[0] == "all") {
+		args = g.GetModuleArgsInOrder(g.AllModulesInOrder)
+	} else {
+		for _, moduleName := range args {
+			err := g.ModuleExists(moduleName)
+			if err != nil {
+				fmt.Println(err)
+				fmt.Println("** restart failed **")
+				return nil //g.Command_EX_ERR
+			}
+		}
+	}
+
+	stop(c, args)
+	start(c, args)
+	return nil //g.Command_EX_OK
+}
---
+++ cmd/tail.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/Cepave/open-falcon-backend/g"
+	"github.com/spf13/cobra"
+)
+
+var Tail = &cobra.Command{
+	Use:   "tail [Module ...]",
+	Short: "Display an Open-Falcon module's log",
+	Long: `
+Display the log of the specified Open-Falcon module.
+A module represents a single node in a cluster.
+
+Modules:
+
+  ` + strings.Join(g.AllModulesInOrder, " "),
+	RunE: tail,
+}
+
+func tail(c *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return c.Usage()
+	}
+	moduleName := args[0]
+	err := g.ModuleExists(moduleName)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("** start failed **")
+		return nil //g.Command_EX_ERR
+	}
+
+	logPath := "./" + moduleName + "/" + g.LogDir + "/" + moduleName + ".log"
+	cmd := exec.Command("tail", "-f", logPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	dir, _ := os.Getwd()
+	cmd.Dir = dir
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("** tail failed **")
+		return nil //g.Command_EX_ERR
+	}
+	return nil //0
+}
