5ea36f8723f576194fb7271f7eba4eeac72ba98f
Minimum <s5550055@yahoo.com.tw>
2016-08-11 16:33:57 +0800

    update version to v5.1.4

--- modules/agent/g/const.go
+++ modules/agent/g/const.go
@@ -10,8 +10,9 @@ import (
 // 5.0.0: 支持通过配置控制是否开启/run接口；收集udp流量数据；du某个目录的大小
 // 5.1.0: 同步插件的时候不再使用checksum机制
 // 5.1.3: Fix config syntax error when deploying
+// 5.1.4: Only trustable ip could access the webpage
 const (
-	VERSION          = "5.1.3"
+	VERSION          = "5.1.4"
 	COLLECT_INTERVAL = time.Second
 	URL_CHECK_HEALTH = "url.check.health"
 	NET_PORT_LISTEN  = "net.port.listen"
