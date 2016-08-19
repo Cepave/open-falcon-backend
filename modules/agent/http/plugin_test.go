a9ae20f67159ebb9948b21c3046f908e5a2390e2
Laurence Chen <humorless@gmail.com>
2016-08-19 04:15:27 +0000

    [OWL-975][agent] add command timout to prevent goroutine blocking

--- modules/agent/http/plugin_test.go
+++ modules/agent/http/plugin_test.go
@@ -1,10 +1,22 @@
 package http
 
-import "testing"
+import (
+	"os/exec"
+	"testing"
+	"time"
+)
 
 func TestDeleteAndCloneRepo(t *testing.T) {
-	out := deleteAndCloneRepo("./plugin", "https://github.com/humorless/openfalcon-plugin.git")
+	out := DeleteAndCloneRepo("./plugin", "https://github.com/humorless/openfalcon-plugin.git")
 	t.Log("test deleteAndCloneRepo: ", out)
-	out = deleteAndCloneRepo("/", "https://github.com/humorless/openfalcon-plugin.git")
+	out = DeleteAndCloneRepo("/", "https://github.com/humorless/openfalcon-plugin.git")
 	t.Log("test deleteAndCloneRepo: ", out)
+}
+
+func TestRunCmdWithTimeout(t *testing.T) {
+	cmd := exec.Command("sleep", "500")
+	t1 := time.Now()
+	RunCmdWithTimeout(cmd, 3)
+	t2 := time.Now()
+	t.Log("Time spent should less than 4 second: ", t2.Sub(t1))
 }
