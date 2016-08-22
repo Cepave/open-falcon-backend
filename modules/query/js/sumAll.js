ccf2607ffc7bc7073b88e62bb143a18ea43b8225
masato25 <help0999@gmail.com>
2016-08-22 13:37:13 +0800

    fixed sumAll.js display issue when only one matched record

--- modules/query/js/sumAll.js
+++ modules/query/js/sumAll.js
@@ -10,24 +10,22 @@ var values = []
 var tmpR = _.filter(input, function(obj){
   return obj.Values.length != 0
 })
+
 if(tmpR.length != 0){
   _.each(tmpR[0].Values, function(val, indx){
     value = { Value: 0,
-              Timestamp: val.Timestamp}
+              Timestamp: val.Timestamp }
     values.push(value)
     return
   })
   output_tmp.Values = values
   //compute
-  if(input.length != 1){
-    _.each(input, function(record){
-      _.each(record.Values, function(v, indx){
-        value = (isNaN(v.Value)? 0 : v.Value)
-        output_tmp.Values[indx].Value += value
-        return
-      })
-      return
+  _.each(input, function(record){
+    _.each(record.Values, function(v, indx){
+      value = (isNaN(v.Value)? 0 : v.Value)
+      return output_tmp.Values[indx].Value += value
     })
-  }
+    return record
+  })
   output = JSON.stringify([output_tmp])
 }
