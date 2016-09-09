1220fe57e23d07ecf8468b1be25e5f608b200326
Laurence Chen <humorless@gmail.com>
2016-09-09 17:16:07 +0800

    [OWL-937][agent] add a agent.alive.30 builtin metric which agent sends it out every 30 sec

--- modules/agent/funcs/agent.go
+++ modules/agent/funcs/agent.go
@@ -7,3 +7,7 @@ import (
 func AgentMetrics() []*model.MetricValue {
 	return []*model.MetricValue{GaugeValue("agent.alive", 1)}
 }
+
+func AgentMetricsThirty() []*model.MetricValue {
+	return []*model.MetricValue{GaugeValue("agent.alive.30", 1)}
+}
