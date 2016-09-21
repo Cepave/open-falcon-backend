a4688322a429a06edb0b5dde1f4012907bf687cd
Mike Lue <mikelue@cepave.com>
2016-09-21 18:24:42 +0800

    [OWL-750] Refactory the existing services to gin framework

--- modules/hbs/http/proc.go
+++ modules/hbs/http/proc.go
@@ -2,17 +2,20 @@ package http
 
 import (
 	"github.com/Cepave/open-falcon-backend/modules/hbs/cache"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func configProcRoutes() {
-	http.HandleFunc("/expressions", func(w http.ResponseWriter, r *http.Request) {
-		RenderDataJson(w, cache.ExpressionCache.Get())
-	})
+func configProcRoutes(router *gin.Engine) {
+	router.GET("/expressions", gin.WrapF(expressions))
+	router.GET("/plugins/", gin.WrapF(plugins))
+}
 
-	http.HandleFunc("/plugins/", func(w http.ResponseWriter, r *http.Request) {
-		hostname := r.URL.Path[len("/plugins/"):]
-		RenderDataJson(w, cache.GetPlugins(hostname))
-	})
+func expressions(w http.ResponseWriter, r *http.Request) {
+	RenderDataJson(w, cache.ExpressionCache.Get())
+}
 
+func plugins(w http.ResponseWriter, r *http.Request) {
+	hostname := r.URL.Path[len("/plugins/"):]
+	RenderDataJson(w, cache.GetPlugins(hostname))
 }
