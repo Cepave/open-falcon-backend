f2ae91556be87d7f11350f2a880be66a8262ed00
chyeh <chyeh@cepave.com>
2016-10-05 10:51:45 +0800

    [OWL-865][nqm-agent] Modification of test
    In this commit, It is modified for testing the cases of undefined
    `name_tag_id` and `group_tag_ids`.

--- modules/nqm-agent/marshal_test.go
+++ modules/nqm-agent/marshal_test.go
@@ -39,32 +39,39 @@ func TestConvToKeyValueString(t *testing.T) {
 }
 
 func TestAssembleTags(t *testing.T) {
-	agent := nqmNodeData{
-		"-1", "-1", "-1", "-1", "11", "12-13-14",
+	agent := []nqmNodeData{
+		{"-1", "-1", "-1", "-1", "11", "12-13-14"},
+		{"-1", "-1", "-1", "-1", "-1", ""},
+		{"-1", "-1", "-1", "-1", "11", "12-13-14"},
+		{"-1", "-1", "-1", "-1", "-1", ""},
 	}
 	target := nqmNodeData{
 		"-2", "-2", "-2", "-2", "21", "22-23-24",
 	}
 	tests := []map[string]string{
 		{"rttmax": "46.5", "rttavg": "14.5", "rttmdev": "-1", "rttmedian": "-1", "pkttransmit": "100", "pktreceive": "100", "rttmin": "8.61"},
+		{"rttmax": "46.5", "rttavg": "14.5", "rttmdev": "-1", "rttmedian": "-1", "pkttransmit": "100", "pktreceive": "100", "rttmin": "8.61"},
+		{"time": "13.6"},
 		{"time": "13.6"},
 	}
 
 	expecteds := []string{
 		"agent-id=-1,agent-isp-id=-1,agent-province-id=-1,agent-city-id=-1,agent-name-tag-id=11,agent-group-tag-ids=12-13-14,target-id=-2,target-isp-id=-2,target-province-id=-2,target-city-id=-2,target-name-tag-id=21,target-group-tag-ids=22-23-24,rttmin=8.61,rttmax=46.5,rttavg=14.5,rttmdev=-1,rttmedian=-1,pkttransmit=100,pktreceive=100,time=",
+		"agent-id=-1,agent-isp-id=-1,agent-province-id=-1,agent-city-id=-1,agent-name-tag-id=-1,agent-group-tag-ids=,target-id=-2,target-isp-id=-2,target-province-id=-2,target-city-id=-2,target-name-tag-id=21,target-group-tag-ids=22-23-24,rttmin=8.61,rttmax=46.5,rttavg=14.5,rttmdev=-1,rttmedian=-1,pkttransmit=100,pktreceive=100,time=",
 		"agent-id=-1,agent-isp-id=-1,agent-province-id=-1,agent-city-id=-1,agent-name-tag-id=11,agent-group-tag-ids=12-13-14,target-id=-2,target-isp-id=-2,target-province-id=-2,target-city-id=-2,target-name-tag-id=21,target-group-tag-ids=22-23-24,rttmin=,rttmax=,rttavg=,rttmdev=,rttmedian=,pkttransmit=,pktreceive=,time=13.6",
+		"agent-id=-1,agent-isp-id=-1,agent-province-id=-1,agent-city-id=-1,agent-name-tag-id=-1,agent-group-tag-ids=,target-id=-2,target-isp-id=-2,target-province-id=-2,target-city-id=-2,target-name-tag-id=21,target-group-tag-ids=22-23-24,rttmin=,rttmax=,rttavg=,rttmdev=,rttmedian=,pkttransmit=,pktreceive=,time=13.6",
 	}
 
 	//t_out := nqmTagsAssembler(target, agent, tests)
 	for i, v := range tests {
-		kv := assembleTags(target, agent, v)
+		kv := assembleTags(target, agent[i], v)
 		strs := strings.Split(kv, ",")
 		for _, str := range strs {
 			if !strings.Contains(expecteds[i], str) {
-				t.Error(expecteds[i], str)
+				t.Error("case:", i, expecteds[i], str)
 			}
 		}
-		t.Log(assembleTags(target, agent, v))
+		t.Log(assembleTags(target, agent[i], v))
 	}
 }
 
