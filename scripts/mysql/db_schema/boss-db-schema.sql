a80a548f8ddbdf08d120cba4209005c98402d462
Don <don@cepave.com>
2016-11-04 15:58:00 +0800

    [OWL-1205][scripts] add boss.idcs table

--- scripts/mysql/db_schema/boss-db-schema.sql
+++ scripts/mysql/db_schema/boss-db-schema.sql
@@ -32,6 +32,20 @@ CREATE TABLE `hosts` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+DROP TABLE IF EXISTS `idcs`;
+CREATE TABLE `idcs` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `popid` int(6) NOT NULL,
+  `idc` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `bandwidth` int(10) DEFAULT NULL,
+  `count` int(6) DEFAULT NULL,
+  `area` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `province` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `city` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `updated` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 DROP TABLE IF EXISTS `ips`;
 CREATE TABLE `ips` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
