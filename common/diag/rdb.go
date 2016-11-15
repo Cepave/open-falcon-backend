b5f58efd88793782324f457e66bd4e7c2105d6cf
Mike Lue <mikelue@cepave.com>
2016-11-15 15:10:29 +0800

    [OWL-750] Add RESTful service for health

---
+++ common/diag/rdb.go
@@ -0,0 +1,47 @@
+package diag
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+)
+
+type RdbDiagnosis struct {
+	Dsn string `json:"dsn"`
+	OpenConnections int `json:"open_connections"`
+	PingResult int `json:"ping_result"`
+	PingMessage string `json:"ping_message"`
+}
+
+// Performs the diagnosis to RDB
+func DiagnoseRdb(dsn string, db *sql.DB) *RdbDiagnosis {
+	var v int
+	err := db.QueryRow("SELECT 0 FROM DUAL").Scan(&v)
+
+	pingResult := 0
+	pingMessage := ""
+	if err != nil {
+		pingResult = 1
+		pingMessage = err.Error()
+	}
+
+	return &RdbDiagnosis {
+		Dsn: hidePasswordOfDsn(dsn),
+		OpenConnections: db.Stats().OpenConnections,
+		PingResult: pingResult,
+		PingMessage: pingMessage,
+	}
+}
+
+func hidePasswordOfDsn(dsn string) string {
+	config, err := mysql.ParseDSN(dsn)
+	if err != nil {
+		return fmt.Sprintf("Cannot parse DSN:[%s]. %v", dsn, err)
+	}
+
+	return fmt.Sprintf(
+		"%s:!hide password!@%s(%s)/%s",
+		config.User, config.Net, config.Addr,
+		config.DBName,
+	)
+}
---
+++ modules/nqm-conf-mng/restful/diag.go
@@ -0,0 +1,25 @@
+package restful
+
+import (
+	"net/http"
+	gin "gopkg.in/gin-gonic/gin.v1"
+	json "gopkg.in/bitly/go-simplejson.v0"
+	"github.com/Cepave/open-falcon-backend/modules/nqm-conf-mng/rdb"
+	"github.com/Cepave/open-falcon-backend/common/diag"
+)
+
+func health(context *gin.Context) {
+	diagRdb := diag.DiagnoseRdb(
+		rdb.DbConfig.Dsn,
+		rdb.DbFacade.SqlDb,
+	)
+
+	jsonResp := json.New()
+	jsonResp.Set("rdb", diagRdb)
+
+	jsonHttp := json.New()
+	jsonHttp.Set("listening", GinConfig.GetAddress())
+	jsonResp.Set("http", jsonHttp)
+
+	context.JSON(http.StatusOK, jsonResp)
+}
