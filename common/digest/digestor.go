6438ede73f758a7c795a2cb24b08ccd1ce5261bd
Mike Lue <mikelue@cepave.com>
2016-12-14 11:58:38 +0800

    [OWL-1167] Add library for digesting on struct

---
+++ common/digest/digestor.go
@@ -0,0 +1,25 @@
+package digest
+
+// Gets the digest of MD5
+type Digestor interface {
+	GetDigest() []byte
+}
+
+// Function object for Digestor
+type DigestorFunc func() []byte
+func (f DigestorFunc) GetDigest() []byte {
+	return f()
+}
+
+type SumFunc func([]byte) []byte
+
+// Sums all of the digestors with customized sum function
+func SumAll(sumFunc SumFunc, rest ...Digestor) []byte {
+	allBytes := make([]byte, 0)
+
+	for _, digestor := range rest {
+		allBytes = append(allBytes, digestor.GetDigest()...)
+	}
+
+	return sumFunc(allBytes)
+}
