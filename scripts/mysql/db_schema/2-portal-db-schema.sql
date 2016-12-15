afd283382e2f3108a6ceb76158d841232c078364
Mike Lue <mikelue@cepave.com>
2016-12-15 10:19:13 +0800

    [OWL-1167] Add index for owl_query table

--- scripts/mysql/db_schema/2-portal-db-schema.sql
+++ scripts/mysql/db_schema/2-portal-db-schema.sql
@@ -1267,7 +1267,8 @@ CREATE TABLE IF NOT EXISTS owl_query (
 	qr_time_access DATETIME NOT NULL,
 	CONSTRAINT unq_owl_query__qr_named_id_qr_md5_content UNIQUE(
 		qr_named_id, qr_md5_content
-	)
+	),
+	INDEX ix_owl_query__qr_time_access (qr_time_access ASC)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
   DEFAULT CHARSET =utf8
   COLLATE =utf8_general_ci;
--- scripts/mysql/dbpatch/change-log/schema-portal/mike-27.sql
+++ scripts/mysql/dbpatch/change-log/schema-portal/mike-27.sql
@@ -7,5 +7,6 @@ CREATE TABLE IF NOT EXISTS owl_query (
 	qr_time_access DATETIME NOT NULL,
 	CONSTRAINT unq_owl_query__qr_named_id_qr_md5_content UNIQUE(
 		qr_named_id, qr_md5_content
-	)
+	),
+	INDEX ix_owl_query__qr_time_access (qr_time_access ASC)
 );
