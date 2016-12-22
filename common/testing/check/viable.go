0965f4ed60827ff1267759ee869b74bf74341e09
Mike Lue <mikelue@cepave.com>
2016-12-22 12:10:51 +0800

    [OWL-1167] Add checking for various situations

---
+++ common/testing/check/viable.go
@@ -0,0 +1,34 @@
+package check
+
+import (
+	"gopkg.in/check.v1"
+	"reflect"
+	"github.com/Cepave/open-falcon-backend/common/utils"
+)
+
+// See utils.ValueExt.IsViable() function
+var ViableValue = viableValue(true)
+
+type viableValue bool
+func (v viableValue) Info() *check.CheckerInfo {
+	return &check.CheckerInfo {
+		Name: "ViableValue",
+		Params: []string{ "obtained", "Need viable" },
+	}
+}
+func (v viableValue) Check(params []interface{}, names []string) (bool, string) {
+	needViable := params[1].(bool)
+
+	checkedValue := reflect.ValueOf(params[0])
+	valueExt := utils.ValueExt(checkedValue)
+
+	valid := valueExt.IsViable()
+
+	if needViable && !valid {
+		return false, "Obtained value should not be nil"
+	} else if !needViable && valid {
+		return false, "Obtained value should be nil"
+	}
+
+	return true, ""
+}
