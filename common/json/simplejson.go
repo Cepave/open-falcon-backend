8300aa5ed79b8de3e74036fa78ec867d2270dc49
Mike Lue <mikelue@cepave.com>
2016-12-22 12:13:06 +0800

    [OWL-1167] Add typed getter on simple-json library

--- common/json/simplejson.go
+++ common/json/simplejson.go
@@ -4,6 +4,48 @@ import (
 	json "github.com/bitly/go-simplejson"
 )
 
+type JsonExt struct {
+	*json.Json
+}
+
+func ToJsonExt(json *json.Json) *JsonExt {
+	return &JsonExt{ json }
+}
+
+func (j *JsonExt) MustInt8() int8 {
+	return int8(j.MustInt64())
+}
+func (j *JsonExt) MustInt16() int16 {
+	return int16(j.MustInt64())
+}
+func (j *JsonExt) MustInt32() int32 {
+	return int32(j.MustInt64())
+}
+
+func (j *JsonExt) MustUint8() uint8 {
+	return uint8(j.MustUint64())
+}
+func (j *JsonExt) MustUint16() uint16 {
+	return uint16(j.MustUint64())
+}
+func (j *JsonExt) MustUint32() uint32 {
+	return uint32(j.MustUint64())
+}
+
+func (j *JsonExt) GetExt(key string) *JsonExt {
+	return &JsonExt{ j.Get(key) }
+}
+func (j *JsonExt) GetIndexExt(index int) *JsonExt {
+	return &JsonExt{ j.GetIndex(index) }
+}
+func (j *JsonExt) GetPathExt(branch ...string) *JsonExt {
+	return &JsonExt{ j.GetPath(branch...) }
+}
+func (j *JsonExt) CheckGetExt(key string) (*JsonExt, bool) {
+	json, check := j.CheckGet(key)
+	return &JsonExt{ json }, check
+}
+
 // Gets the JSON
 //
 // This method would panic if the JSON cannot be marshalled
