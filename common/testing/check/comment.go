cbb620baf7166162bf4ac351a6c2e20883b94d73
Mike Lue <mikelue@cepave.com>
2016-12-28 17:34:07 +0800

    [OWL-1167] Add comment and logging for certain data

---
+++ common/testing/check/comment.go
@@ -0,0 +1,13 @@
+package check
+
+import (
+	. "gopkg.in/check.v1"
+)
+
+func LogTestCase(c *C, testCase interface{}) {
+	c.Logf("Test Case: [%v]", testCase)
+}
+
+func TestCaseComment(index int) CommentInterface {
+	return Commentf("Test Case: %d", index + 1)
+}
