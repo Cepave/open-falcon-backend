0a4e8b82b7c312f054d9a17bbd19801791d63ca0
Mike Lue <mikelue@cepave.com>
2017-02-16 15:47:42 +0800

    [OWL-1418] Add building of error handler on panic

--- common/rpc/error_handler.go
+++ common/rpc/error_handler.go
@@ -2,7 +2,10 @@ package rpc
 
 import (
 	"fmt"
+
 	log "github.com/Cepave/open-falcon-backend/common/logruslog"
+
+	"github.com/Cepave/open-falcon-backend/common/utils"
 )
 
 var logger = log.NewDefaultLogger("WARN")
@@ -10,18 +13,16 @@ var logger = log.NewDefaultLogger("WARN")
 // Builds defer function for handling panic,
 // and sets the value of error into error object
 func HandleError(err *error) func() {
-	return func() {
-		p := recover()
-		if p != nil {
+	return utils.PanicToError(
+		err,
+		func(p interface{}) error {
 			logger.Errorf("Panic in RPC(GoLang): %v", p)
 
-			switch errObject := p.(type) {
-			case error:
-				*err = errObject
-			default:
-				newErr := fmt.Errorf("Has error on RPC: %v", errObject)
-				*err = newErr
+			if errObject, ok := p.(error); ok {
+				return errObject
 			}
-		}
-	}
+
+			return fmt.Errorf("Has error on RPC: %v", p)
+		},
+	)
 }
---
+++ common/utils/errors.go
@@ -0,0 +1,77 @@
+//
+// Error Handler
+//
+// PanicToError() could be used to transfer a panic code to customized error object
+//
+// PanicToSimpleError() could be used to transfer a panic code to simple error object
+//
+// 	func your_func() (err error) {
+// 		defer PanicToSimpleError(&err)()
+//
+// 		// Code may cause panic...
+// 	}
+//
+// You can wrap a function to a function with error returned:
+//
+// 	func your_func(int) { /**/ }
+//
+// 	errFunc := PanicToSimpleErrorWrapper(
+// 		func() {
+// 			your_func(var_1)
+// 		},
+// 	)
+//
+// 	err := errFunc()
+package utils
+
+import (
+	"fmt"
+)
+
+// Defines the converter from converting any value to error object
+type ErrorConverter func(p interface{}) error
+
+// Converts the panic content to error object
+//
+// 	err - The holder of error object
+// 	errConverter - The builder function for converting non-error object to error object
+func PanicToError(err *error, errConverter ErrorConverter) func() {
+	return func() {
+		p := recover()
+		if p == nil {
+			return
+		}
+
+		*err = errConverter(p)
+	}
+}
+
+// Converts the panic content to error object by SimpleErrorConverter()
+func PanicToSimpleError(err *error) func() {
+	return PanicToError(err, SimpleErrorConverter)
+}
+
+// Simple converter for converting non-error object to error object by:
+//
+// 	fmt.Errorf("%v", object)
+func SimpleErrorConverter(p interface{}) error {
+	if errObject, ok := p.(error); ok {
+		return errObject
+	}
+
+	return fmt.Errorf("%v", p)
+}
+
+// Convert a lambda function to function with error returned
+func PanicToErrorWrapper(mainFunc func(), errConverter ErrorConverter) func() error {
+	return func() (err error) {
+		defer PanicToError(&err, errConverter)()
+		mainFunc()
+		return
+	}
+}
+
+// Convert a lambda function to function with error returned(by SimpleErrorConverter)
+func PanicToSimpleErrorWrapper(mainFunc func()) func() error {
+	return PanicToErrorWrapper(mainFunc, SimpleErrorConverter)
+}
