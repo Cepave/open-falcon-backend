051fcda236503fe488e1092f99bf37f6cbea9502
Masato25 <help0999@gmail.com>
2017-03-08 15:47:24 +0800

    user general way to set web api headers.

--- modules/api/app/utils/auth_middle.go
+++ modules/api/app/utils/auth_middle.go
@@ -25,13 +25,6 @@ func AuthSessionMidd(c *gin.Context) {
 
 func CORS() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		context.Writer.Header().Add("Access-Control-Allow-Origin", "*")
-		context.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		context.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		context.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Apitoken")
-		context.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-		context.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-
 		if context.Request.Method == "OPTIONS" {
 			context.AbortWithStatus(200)
 		} else {
