3bd2408420d0072cdffacf9dd0f60ae2fa11a272
Mike Lue <mikelue@cepave.com>
2017-03-09 15:43:21 +0800

    [OWL-1554] Fix short-time bug for query UUID

--- common/db/owl/query.go
+++ common/db/owl/query.go
@@ -1,7 +1,6 @@
 package owl
 
 import (
-	"database/sql"
 	"github.com/satori/go.uuid"
 	model "github.com/Cepave/open-falcon-backend/common/model/owl"
 	"github.com/Cepave/open-falcon-backend/common/db"
@@ -17,55 +16,35 @@ func AddOrRefreshQuery(query *model.Query, accessTime time.Time) {
 
 // Updates access time of a query or adding new one
 func UpdateAccessTimeOrAddNewOne(query *model.Query, accessTime time.Time) {
-	result := db.ToResultExt(DbFacade.SqlxDbCtrl.NamedExec(
-		`
-		UPDATE owl_query
-		SET qr_time_access = :access_time
-		WHERE qr_uuid = :uuid
-		`,
-		map[string]interface{} {
-			"access_time": accessTime,
-			"uuid": query.Uuid,
-		},
-	))
-
-	if result.RowsAffected() == 0 {
-		AddOrRefreshQuery(query, accessTime)
+	if isQueryExistingByUuid(query.Uuid) {
+		updateAccessTimeByUuid(
+			query.Uuid, accessTime,
+		)
+		return
 	}
+
+	AddOrRefreshQuery(query, accessTime)
 }
 
+// It is possible that the access times of two requests are very closed(less than 1 second)
 func LoadQueryByUuidAndUpdateAccessTime(name string, uuid uuid.UUID, accessTime time.Time) *model.Query {
-	result := db.ToResultExt(DbFacade.SqlxDbCtrl.NamedExec(
-		`
-		UPDATE owl_query
-		SET qr_time_access = :access_time
-		WHERE qr_uuid = :uuid
-		`,
-		map[string]interface{} {
-			"access_time": accessTime,
-			"uuid": db.DbUuid(uuid),
-		},
-	))
-
-	if result.RowsAffected() == 0 {
-		return nil
-	}
+	updateAccessTimeByUuid(
+		db.DbUuid(uuid), accessTime,
+	)
 
 	var queryData model.Query
-	err := DbFacade.SqlxDb.QueryRowx(
+
+	if !DbFacade.SqlxDbCtrl.GetOrNoRow(
+		&queryData,
 		`
 		SELECT qr_uuid, qr_named_id, qr_content, qr_md5_content
 		FROM owl_query
 		WHERE qr_uuid = ?
 		`,
 		db.DbUuid(uuid),
-	).StructScan(&queryData)
-
-	if err == sql.ErrNoRows {
+	) {
 		return nil
 	}
-
-	db.PanicIfError(err)
 
 	return &queryData
 }
@@ -126,5 +105,30 @@ func (queryTx *addOrRefreshQueryTx) loadUuid(txExt *sqlxExt.TxExt) {
 			queryObject.Md5Content,
 		},
 		&queryObject.Uuid,
+	)
+}
+
+func updateAccessTimeByUuid(uuid db.DbUuid, accessTime time.Time) {
+	DbFacade.SqlxDbCtrl.NamedExec(
+		`
+		UPDATE owl_query
+		SET qr_time_access = :access_time
+		WHERE qr_uuid = :uuid
+		`,
+		map[string]interface{} {
+			"access_time": accessTime,
+			"uuid": uuid,
+		},
+	)
+}
+func isQueryExistingByUuid(uuid db.DbUuid) bool {
+	return DbFacade.SqlxDbCtrl.GetOrNoRow(
+		&struct { Uuid db.DbUuid `db:"qr_uuid"` }{},
+		`
+		SELECT qr_uuid
+		FROM owl_query
+		WHERE qr_uuid = ?
+		`,
+		uuid,
 	)
 }
