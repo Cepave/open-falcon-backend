ee0e35de0d1f0a617bcefcd586c502ea90964960
Mike Lue <mikelue@cepave.com>
2017-03-28 15:52:40 +0800

    [OWL-1594] Add checking of least value on cache timeout(non-debug mode)

--- modules/hbs/rpc/package.go
+++ modules/hbs/rpc/package.go
@@ -10,6 +10,10 @@ import (
 var logger = log.NewDefaultLogger("INFO")
 
 func InitPackage(config *viper.Viper) {
+	initNqmConfig(config)
+}
+
+func initNqmConfig(config *viper.Viper) {
 	config.SetDefault("nqm.queue_size.refresh_agent_ping_list", 8)
 	config.SetDefault("nqm.cache_minutes.agent_ping_list", 20)
 
@@ -17,6 +21,15 @@ func InitPackage(config *viper.Viper) {
 		QueueSizeOfRefreshCacheOfPingList: config.GetInt("nqm.queue_size.refresh_agent_ping_list"),
 		CacheTimeoutMinutes: config.GetInt("nqm.cache_minutes.agent_ping_list"),
 	}
+
+	/**
+	 * If the mode is not in debug, the least timeout is 10 minutes
+	 */
+	if !config.GetBool("debug") && config.GetInt("nqm.cache_minutes.agent_ping_list") < 5 {
+		nqmConfig.CacheTimeoutMinutes = 5
+	}
+	// :~)
+
 	nqmAgentHbsService = nqmService.NewAgentHbsService(nqmConfig)
 
 	logger.Infof("[NQM] Ping list of agent. Timeout: %d minutes. Queue Size: %d",
