8669d9da7e8eec8182d010fda30d675c24c5d0fa
chyeh <chyeh@cepave.com>
2017-04-06 15:29:37 +0800

    [OWL-1332][common] Fix `ORDER BY`

--- common/db/nqm/pingtask.go
+++ common/db/nqm/pingtask.go
@@ -1,6 +1,7 @@
 package nqm
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/jinzhu/gorm"
@@ -10,6 +11,8 @@ import (
 	gormExt "github.com/Cepave/open-falcon-backend/common/gorm"
 	commonModel "github.com/Cepave/open-falcon-backend/common/model"
 	nqmModel "github.com/Cepave/open-falcon-backend/common/model/nqm"
+	tb "github.com/Cepave/open-falcon-backend/common/textbuilder"
+	sqlb "github.com/Cepave/open-falcon-backend/common/textbuilder/sql"
 )
 
 type addAgentPingtaskTx struct {
@@ -142,89 +145,93 @@ func ListPingtasks(query *nqmModel.PingtaskQuery, paging commonModel.Paging) ([]
 	var result []*nqmModel.PingtaskView
 
 	var funcTxLoader gormExt.TxCallbackFunc = func(txGormDb *gorm.DB) commonDb.TxFinale {
-		/**
-		 * Retrieves the page of data
-		 */
-		var selectPingtask = txGormDb.Model(&nqmModel.PingtaskView{}).
-			Select(`SQL_CALC_FOUND_ROWS
-				pt_id, pt_period, pt_name, pt_enable, pt_comment,
-        COUNT(DISTINCT ag.ag_id) AS pt_num_of_enabled_agents,
-        GROUP_CONCAT(DISTINCT isp.isp_id ORDER BY isp_id ASC SEPARATOR ',') AS pt_isp_filter_ids,
-        GROUP_CONCAT(DISTINCT isp.isp_name ORDER BY isp_id ASC SEPARATOR '\0') AS pt_isp_filter_names,
-        GROUP_CONCAT(DISTINCT pv.pv_id ORDER BY pv_id ASC SEPARATOR ',') AS pt_province_filter_ids,
-        GROUP_CONCAT(DISTINCT pv.pv_name ORDER BY pv_id ASC SEPARATOR '\0') AS pt_province_filter_names,
-        GROUP_CONCAT(DISTINCT ct.ct_id ORDER BY ct_id ASC SEPARATOR ',') AS pt_city_filter_ids,
-        GROUP_CONCAT(DISTINCT ct.ct_pv_id ORDER BY ct_id ASC SEPARATOR ',') AS pt_city_filter_pv_ids,
-        GROUP_CONCAT(DISTINCT ct.ct_name ORDER BY ct_id ASC SEPARATOR '\0') AS pt_city_filter_names,
-        GROUP_CONCAT(DISTINCT nt.nt_id ORDER BY nt_id ASC SEPARATOR ',') AS pt_name_tag_filter_ids,
-        GROUP_CONCAT(DISTINCT nt.nt_value ORDER BY nt_id ASC SEPARATOR '\0') AS pt_name_tag_filter_values,
-        GROUP_CONCAT(DISTINCT gt.gt_id ORDER BY gt_id ASC SEPARATOR ',') AS pt_group_tag_filter_ids,
-        GROUP_CONCAT(DISTINCT gt.gt_name ORDER BY gt_id ASC SEPARATOR '\0') AS pt_group_tag_filter_names
-			`).
-			Joins(`
-				LEFT JOIN
-				nqm_agent_ping_task AS apt
-				ON pt_id = apt.apt_pt_id
-				LEFT JOIN
-				nqm_agent AS ag
-				ON apt.apt_ag_id = ag.ag_id AND ag.ag_status=true
-				LEFT JOIN
-				nqm_pt_target_filter_isp AS tfisp
-				ON pt_id = tfisp.tfisp_pt_id
-				LEFT JOIN
-				owl_isp AS isp
-				ON tfisp.tfisp_isp_id = isp.isp_id
-				LEFT JOIN
-				nqm_pt_target_filter_province AS tfpv
-				ON pt_id = tfpv.tfpv_pt_id
-				LEFT JOIN
-				owl_province AS pv
-				ON tfpv.tfpv_pv_id = pv.pv_id
-				LEFT JOIN
-				nqm_pt_target_filter_city AS tfct
-				ON pt_id = tfct.tfct_pt_id
-				LEFT JOIN
-				owl_city AS ct
-				ON tfct.tfct_ct_id = ct.ct_id
-				LEFT JOIN
-				nqm_pt_target_filter_name_tag AS tfnt
-				ON pt_id = tfnt.tfnt_pt_id
-				LEFT JOIN
-				owl_name_tag AS nt
-				ON tfnt.tfnt_nt_id = nt.nt_id
-				LEFT JOIN
-				nqm_pt_target_filter_group_tag AS tfgt
-				ON pt_id = tfgt.tfgt_pt_id
-				LEFT JOIN
-				owl_group_tag AS gt
-				ON tfgt.tfgt_gt_id = gt.gt_id
-			`).
-			Limit(paging.Size).
-			Group(`
-				pt_id, pt_period, pt_name, pt_enable, pt_comment
-			`).
-			Order(buildSortingClauseOfPingtasks(&paging)).
-			Offset(paging.GetOffset())
+		sqlStr := `SELECT SQL_CALC_FOUND_ROWS
+			pt_id, pt_period, pt_name, pt_enable, pt_comment,
+			COUNT(DISTINCT ag.ag_id) AS pt_num_of_enabled_agents,
+			GROUP_CONCAT(DISTINCT isp.isp_id ORDER BY isp_id ASC SEPARATOR ',') AS pt_isp_filter_ids,
+			GROUP_CONCAT(DISTINCT isp.isp_name ORDER BY isp_id ASC SEPARATOR '\0') AS pt_isp_filter_names,
+			GROUP_CONCAT(DISTINCT pv.pv_id ORDER BY pv_id ASC SEPARATOR ',') AS pt_province_filter_ids,
+			GROUP_CONCAT(DISTINCT pv.pv_name ORDER BY pv_id ASC SEPARATOR '\0') AS pt_province_filter_names,
+			GROUP_CONCAT(DISTINCT ct.ct_id ORDER BY ct_id ASC SEPARATOR ',') AS pt_city_filter_ids,
+			GROUP_CONCAT(DISTINCT ct.ct_pv_id ORDER BY ct_id ASC SEPARATOR ',') AS pt_city_filter_pv_ids,
+			GROUP_CONCAT(DISTINCT ct.ct_name ORDER BY ct_id ASC SEPARATOR '\0') AS pt_city_filter_names,
+			GROUP_CONCAT(DISTINCT nt.nt_id ORDER BY nt_id ASC SEPARATOR ',') AS pt_name_tag_filter_ids,
+			GROUP_CONCAT(DISTINCT nt.nt_value ORDER BY nt_id ASC SEPARATOR '\0') AS pt_name_tag_filter_values,
+			GROUP_CONCAT(DISTINCT gt.gt_id ORDER BY gt_id ASC SEPARATOR ',') AS pt_group_tag_filter_ids,
+			GROUP_CONCAT(DISTINCT gt.gt_name ORDER BY gt_id ASC SEPARATOR '\0') AS pt_group_tag_filter_names
+			From nqm_ping_task
+			LEFT JOIN
+			nqm_agent_ping_task AS apt
+			ON pt_id = apt.apt_pt_id
+			LEFT JOIN
+			nqm_agent AS ag
+			ON apt.apt_ag_id = ag.ag_id AND ag.ag_status=true
+			LEFT JOIN
+			nqm_pt_target_filter_isp AS tfisp
+			ON pt_id = tfisp.tfisp_pt_id
+			LEFT JOIN
+			owl_isp AS isp
+			ON tfisp.tfisp_isp_id = isp.isp_id
+			LEFT JOIN
+			nqm_pt_target_filter_province AS tfpv
+			ON pt_id = tfpv.tfpv_pt_id
+			LEFT JOIN
+			owl_province AS pv
+			ON tfpv.tfpv_pv_id = pv.pv_id
+			LEFT JOIN
+			nqm_pt_target_filter_city AS tfct
+			ON pt_id = tfct.tfct_pt_id
+			LEFT JOIN
+			owl_city AS ct
+			ON tfct.tfct_ct_id = ct.ct_id
+			LEFT JOIN
+			nqm_pt_target_filter_name_tag AS tfnt
+			ON pt_id = tfnt.tfnt_pt_id
+			LEFT JOIN
+			owl_name_tag AS nt
+			ON tfnt.tfnt_nt_id = nt.nt_id
+			LEFT JOIN
+			nqm_pt_target_filter_group_tag AS tfgt
+			ON pt_id = tfgt.tfgt_pt_id
+			LEFT JOIN
+			owl_group_tag AS gt
+			ON tfgt.tfgt_gt_id = gt.gt_id
+			%s
+			GROUP BY pt_id, pt_period, pt_name, pt_enable, pt_comment
+			ORDER BY %s
+			Limit %d, %d
+		`
+
+		var conditions []tb.TextGetter
+		var sqlParams []interface{}
 
 		if query.Period != "" {
-			selectPingtask = selectPingtask.Where("pt_period = ?", query.Period)
+			conditions = append(conditions, tb.Dsl.S("pt_period = ?"))
+			sqlParams = append(sqlParams, query.Period)
 		}
 		if query.Name != "" {
-			selectPingtask = selectPingtask.Where("pt_name LIKE ?", query.Name+"%")
+			conditions = append(conditions, tb.Dsl.S("pt_name LIKE ?"))
+			sqlParams = append(sqlParams, query.Name+"%")
 		}
 		if ena, err := strconv.ParseBool(query.Enable); query.Enable != "" && err == nil {
-			selectPingtask = selectPingtask.Where("pt_enable = ?", ena)
+			conditions = append(conditions, tb.Dsl.S("pt_enable = ?"))
+			sqlParams = append(sqlParams, ena)
 		}
 		if query.Comment != "" {
-			selectPingtask = selectPingtask.Where("pt_comment LIKE ?", query.Comment+"%")
+			conditions = append(conditions, tb.Dsl.S("pt_comment LIKE ?"))
+			sqlParams = append(sqlParams, query.Comment+"%")
 		}
 		if query.NumOfEnabledAgents != "" {
-			selectPingtask = selectPingtask.Where("pt_num_of_enabled_agents = ?", query.NumOfEnabledAgents)
+			conditions = append(conditions, tb.Dsl.S("pt_num_of_enabled_agents = ?"))
+			sqlParams = append(sqlParams, query.NumOfEnabledAgents)
 		}
-		// :~)
 
+		whereClause := sqlb.Where(sqlb.And(conditions...))
+
+		sqlStr = fmt.Sprintf(sqlStr, whereClause.String(), buildSortingClauseOfPingtasks(&paging), paging.GetOffset(), paging.Size)
+
+		var selectPingtask = txGormDb.Model(&nqmModel.PingtaskView{}).Raw(sqlStr, sqlParams...)
 		gormExt.ToDefaultGormDbExt(selectPingtask.Find(&result)).PanicIfError()
-
 		return commonDb.TxCommit
 	}
 
