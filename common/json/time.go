8c1f954420e8d121ec5975cf6b1541fafab7cb4f
chyeh <chyeh@cepave.com>
2017-05-03 16:16:52 +0800

    [OWL-1667][common] Add field `timestamp`

--- common/json/time.go
+++ common/json/time.go
@@ -1,13 +1,16 @@
 package json
 
 import (
+	"database/sql/driver"
 	"fmt"
+	"strconv"
 	"time"
 )
 
 // This type of time would be serialized to UNIX time for MarshalJSON()
 type JsonTime time.Time
 
+// MarshalJSON does the serialization of UNIX timestamp
 func (t JsonTime) MarshalJSON() ([]byte, error) {
 	jsonResult := "null"
 
@@ -17,4 +20,18 @@ func (t JsonTime) MarshalJSON() ([]byte, error) {
 	}
 
 	return ([]byte)(jsonResult), nil
+}
+
+// UnmarshalJSON does the deserialization of UNIX timestamp
+func (t *JsonTime) UnmarshalJSON(data []byte) error {
+	i, err := strconv.ParseInt(string(data), 10, 64)
+	if err != nil {
+		return fmt.Errorf("Cannot parse timestamp string: %s\n", data)
+	}
+	*t = JsonTime(time.Unix(i, 0))
+	return nil
+}
+
+func (t JsonTime) Value() (driver.Value, error) {
+	return time.Time(t).Unix(), nil
 }
