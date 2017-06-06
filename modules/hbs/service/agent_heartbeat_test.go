786a9bdf2d337d153df46b710b82b95d11b65bd2
Mike Lue <mikelue@cepave.com>
2017-06-06 15:27:17 +0800

    [OWL-1695] Refactoring testing on put()

--- modules/hbs/service/agent_heartbeat_test.go
+++ modules/hbs/service/agent_heartbeat_test.go
@@ -12,58 +12,64 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Test AgentHeartbeat service", func() {
+type fakeHeartbeat struct {
+	rowsAffectedCnt int
+	alwaysFail bool
+}
+func (a *fakeHeartbeat) calling(agents []*model.AgentHeartbeat) (int64, int64) {
+	a.rowsAffectedCnt += len(agents)
+
+	if a.alwaysFail {
+		return int64(len(agents)), 0
+	} else {
+		return 0, int64(len(agents))
+	}
+}
+
+var _ = Describe("Test put() of AgentHeartbeat service", func() {
 	var (
-		caseNumber            int           = 0
-		sleepTime             time.Duration = time.Second / 2
 		agentHeartbeatService *AgentHeartbeatService
-		data                  *commonModel.AgentReportRequest
-		heartbeatConfig       *commonQueue.Config = &commonQueue.Config{
-			Num: 16,
-			Dur: 100 * time.Millisecond,
-		}
-		fakeHeartbeatCall = func(agents []*model.AgentHeartbeat) (rowsAffectedCnt int64, agentsDroppedCnt int64) {
-			rowsAffectedCnt = int64(len(agents))
-			return rowsAffectedCnt, 0
-		}
+		heartbeatImpl *fakeHeartbeat
 	)
 
 	BeforeEach(func() {
-		agentHeartbeatService = NewAgentHeartbeatService(heartbeatConfig)
-		agentHeartbeatService.heartbeatCall = fakeHeartbeatCall
-		sampleNumber := strconv.Itoa(caseNumber)
-		data = &commonModel.AgentReportRequest{
-			Hostname:      "agentHeartbeatService-" + sampleNumber,
-			IP:            "127.0.0." + sampleNumber,
-			AgentVersion:  "0.0." + sampleNumber,
-			PluginVersion: "12345abcde" + sampleNumber,
-		}
-		caseNumber++
+		agentHeartbeatService = NewAgentHeartbeatService(
+			&commonQueue.Config{ Num: 16 },
+		)
+
+		heartbeatImpl = &fakeHeartbeat{ alwaysFail: false }
+		agentHeartbeatService.heartbeatCall = heartbeatImpl.calling
+	})
+	AfterEach(func() {
+		agentHeartbeatService.Stop()
 	})
 
-	Describe("Test Put() method", func() {
-		Context("when service is not running", func() {
-			It("should not add data", func() {
-				agentHeartbeatService.Put(data)
-				Expect(agentHeartbeatService.CurrentSize()).To(Equal(0))
-
-				time.Sleep(sleepTime)
-				Expect(agentHeartbeatService.CumulativeAgentsPut()).To(Equal(int64(0)))
-			})
+	Context("when service is not running", func() {
+		It("should not add data", func() {
+			agentHeartbeatService.Put(generateRandomHeartbeat())
+			Expect(agentHeartbeatService.CumulativeAgentsPut()).To(Equal(int64(0)))
+			Expect(agentHeartbeatService.CurrentSize()).To(Equal(0))
 		})
+	})
 
-		Context("when service is running", func() {
-			It("should add data", func() {
-				agentHeartbeatService.Start()
-				agentHeartbeatService.Put(data)
-				Expect(agentHeartbeatService.CurrentSize()).To(Equal(1))
+	Context("when service is running", func() {
+		It("should add data", func() {
+			agentHeartbeatService.Start()
+			agentHeartbeatService.Put(generateRandomHeartbeat())
 
-				time.Sleep(sleepTime)
-				Expect(agentHeartbeatService.CumulativeAgentsPut()).To(Equal(int64(1)))
-			})
+			Expect(agentHeartbeatService.CumulativeAgentsPut()).To(Equal(int64(1)))
 		})
 	})
 })
+
+func generateRandomHeartbeat() *commonModel.AgentReportRequest {
+	return &commonModel.AgentReportRequest{
+		Hostname:      "agentHeartbeatService-",
+		IP:            "127.0.0.",
+		AgentVersion:  "0.0.",
+		PluginVersion: "12345abcde",
+	}
+}
 
 var _ = Describe("Original Test AgentHeartbeat service", func() {
 	var (
