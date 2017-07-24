9bd97ac4e960972891bf9a7357d8b7273c2c1e39
crosserclaws <crosserclaws@gmail.com>
2017-07-24 12:07:35 +0800

    [OWL-1621] Better handling when parsing an illegal url. Add unit tests for numerous cases.

--- modules/hbs/service/package.go
+++ modules/hbs/service/package.go
@@ -28,13 +28,17 @@ func NewSlingBase() *sling.Sling {
 }
 
 func resolveUrl(host string, resource string) string {
+	if host == "" {
+		logger.Panicln("Empty host url of mysql_api.")
+	}
+
 	base, err := url.Parse(host)
 	if err != nil {
-		logger.Errorln(err)
+		logger.Panicf("Cannot parse host of mysql_api: %s. Error: %v\n", host, err)
 	}
 	ref, err := url.Parse(resource)
 	if err != nil {
-		logger.Errorln(err)
+		logger.Panicf("Cannot parse resource of mysql_api: %s. Error: %v\n", resource, err)
 	}
 
 	return base.ResolveReference(ref).String()
--- modules/hbs/service/package_test.go
+++ modules/hbs/service/package_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	"testing"
@@ -11,3 +12,29 @@ func TestByGinkgo(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Service Suite")
 }
+
+var _ = Describe("[Unit] Test resolveUrl(...)", func() {
+	DescribeTable("should panic when",
+		func(host string, resource string) {
+			Expect(
+				func() {
+					_ = resolveUrl(host, resource)
+				},
+			).To(Panic())
+		},
+		Entry("Empty host url", "", ""),
+		Entry("Invalid scheme", "h@ttp://www.owl.com", "mysqlapi"),
+	)
+
+	DescribeTable("should parse successfully when",
+		func(host, resource, expectedURL string) {
+			url := resolveUrl(host, resource)
+			Expect(url).To(Equal(expectedURL))
+		},
+		Entry("No tailing slash", "http://owl.com", "mysqlapi", "http://owl.com/mysqlapi"),
+		Entry("1 tailing slash at host", "http://owl.com/", "mysqlapi", "http://owl.com/mysqlapi"),
+		Entry("1 tailing slash at resource", "http://owl.com", "/mysqlapi", "http://owl.com/mysqlapi"),
+		Entry("2 tailing slash", "http://owl.com/", "/mysqlapi", "http://owl.com/mysqlapi"),
+		Entry("IP:port with no tailing slash", "http://127.0.0.1:5566", "mysqlapi", "http://127.0.0.1:5566/mysqlapi"),
+	)
+})
