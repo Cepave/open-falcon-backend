b15a40d0218440dce3fdaf2be4926fe1e8f4aa2d
crosserclaws <crosserclaws@gmail.com>
2017-07-25 17:19:58 +0800

    [OWL-1621] Fix bug of making assertion in goroutine. Failing before defer channel causes deadlock. Create an extra go routine to avoid non-recover panic due to IoC.

--- modules/hbs/rpc/agent_stress_test.go
+++ modules/hbs/rpc/agent_stress_test.go
@@ -43,21 +43,26 @@ var _ = Describe("[Stress] Test Agent.ReportStatus in HBS", ginkgoJsonRpc.NeedJs
 
 				<-routines
 
-				go ginkgoJsonRpc.OpenClient(func(jsonRpcClient *rpc.Client) {
+				go func() {
 					defer func() {
 						routines <- true
 					}()
+					defer GinkgoRecover()
 
-					err := jsonRpcClient.Call(
-						"Agent.ReportStatus", request, &resp,
-					)
+					ginkgoJsonRpc.OpenClient(func(jsonRpcClient *rpc.Client) {
 
-					if err != nil || resp.Code == 1 {
-						GinkgoT().Errorf("[%s] Has error: %v", request.AgentVersion, err)
-					} else {
-						GinkgoT().Logf("[%s/%d] Success.", request.PluginVersion, numberOfFakeAgents)
-					}
-				})
+						err := jsonRpcClient.Call(
+							"Agent.ReportStatus", request, &resp,
+						)
+
+						if err != nil || resp.Code == 1 {
+							GinkgoT().Errorf("[%s] Has error: %v", request.AgentVersion, err)
+						} else {
+							GinkgoT().Logf("[%s/%d] Success.", request.PluginVersion, numberOfFakeAgents)
+						}
+					})
+				}()
+
 			}
 
 			for i := 0; i < numberOfGoRoutines; i++ {
