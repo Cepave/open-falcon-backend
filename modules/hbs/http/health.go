0548ce3de835ceeea24653d9421efe118241034f
crosserclaws <crosserclaws@gmail.com>
2017-08-31 15:27:03 +0800

    [OWL-1866] Simplify the getHealth function.

--- modules/hbs/http/health.go
+++ modules/hbs/http/health.go
@@ -10,6 +10,38 @@ import (
 	"gopkg.in/h2non/gentleman.v2"
 )
 
+func getHealth(c *gin.Context) {
+	mysqlInfo := fetchMysqlApiView(service.MysqlApiUrl)
+	httpInfo := g.Config().Http
+	rpcInfo := &g.RpcView{g.Config().Listen}
+	heartbeatService := rpc.AgentHeartbeatService
+
+	healthInfo := &g.HealthView{
+		mysqlInfo,
+		httpInfo,
+		rpcInfo,
+		&g.FalconAgentView{
+			&g.HeartbeatView{
+				CurrentSize:         heartbeatService.CurrentSize(),
+				CumulativeReceived:  heartbeatService.CumulativeAgentsPut(),
+				CumulativeDropped:   heartbeatService.CumulativeAgentsDropped(),
+				CumulativeProcessed: heartbeatService.CumulativeRowsAffected(),
+			},
+		},
+	}
+	c.JSON(http.StatusOK, healthInfo)
+}
+
+func fetchMysqlApiView(apiUrl string) *g.MysqlApiView {
+	req := gentleman.New().BaseURL(apiUrl).Path("/health").Head()
+	res, err := req.Do()
+	return &g.MysqlApiView{
+		Address:     apiUrl,
+		PingResult:  Btoi(res.Ok),
+		PingMessage: Etos(err),
+	}
+}
+
 func Btoi(b bool) int {
 	if b {
 		return 1
@@ -22,31 +54,4 @@ func Etos(e error) string {
 		return e.Error()
 	}
 	return ""
-}
-
-func getHealth(c *gin.Context) {
-	apiUrl := service.MysqlApiUrl
-	heartbeatService := rpc.AgentHeartbeatService
-	req := gentleman.New().BaseURL(apiUrl).Path("/health").Head()
-	res, err := req.Do()
-	rpcInfo := g.Config().Listen
-	httpInfo := g.Config().Http
-	healthInfo := g.HealthView{
-		&g.MysqlApiView{
-			Address:     apiUrl,
-			PingResult:  Btoi(res.Ok),
-			PingMessage: Etos(err),
-		},
-		httpInfo,
-		&g.RpcView{rpcInfo},
-		&g.FalconAgentView{
-			&g.HeartbeatView{
-				CurrentSize:         heartbeatService.CurrentSize(),
-				CumulativeReceived:  heartbeatService.CumulativeAgentsPut(),
-				CumulativeDropped:   heartbeatService.CumulativeAgentsDropped(),
-				CumulativeProcessed: heartbeatService.CumulativeRowsAffected(),
-			},
-		},
-	}
-	c.JSON(http.StatusOK, healthInfo)
 }
