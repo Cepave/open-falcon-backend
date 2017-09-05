f087f03bd4b322f283dd8b0bd8717bfeef903b72
Kordan Ou <kordan@hitripod.com>
2017-09-05 18:07:13 +0800

    fix package name used by common/sdk

--- common/sdk/sender/linkedlist.go
+++ common/sdk/sender/linkedlist.go
@@ -18,7 +18,7 @@ import (
 	"container/list"
 	"sync"
 
-	"github.com/open-falcon/falcon-plus/common/model"
+	"github.com/Cepave/open-falcon-backend/common/model"
 )
 
 type SafeLinkedList struct {
--- common/sdk/sender/make.go
+++ common/sdk/sender/make.go
@@ -17,7 +17,7 @@ package sender
 import (
 	"time"
 
-	"github.com/open-falcon/falcon-plus/common/model"
+	"github.com/Cepave/open-falcon-backend/common/model"
 )
 
 func MakeMetaData(endpoint, metric, tags string, val interface{}, counterType string, step_and_ts ...int64) *model.JsonMetaData {
