ad772aeb11688ee319e97e4b1ee51a8e42c5ddb5
crosserclaws <crosserclaws@gmail.com>
2017-11-24 17:05:06 +0800

    [OWL-1997] Refactor first 3 cases.

--- common/db/owl/schedule_test.go
+++ common/db/owl/schedule_test.go
@@ -12,34 +12,59 @@ var _ = Describe("Tests AcquireLock(...)", itSkip.PrependBeforeEach(func() {
 
 	var (
 		defaultSchedule     *Schedule
-		defaultTimeout      = 0
-		now                 time.Time
+		defaultTimeout      = 2
+		defaultNow          time.Time
 		defaultScheduleName = "test-schedule-test"
 
-		lockTable     OwlSchedule
-		logTable      OwlScheduleLog
-		selectLockSql = `
-			SELECT *
-			FROM owl_schedule
-			WHERE sch_name = ?
-		`
-		selectLogSql = `
-			SELECT *
-			FROM owl_schedule_log
-			WHERE sl_sch_id = ?
-			ORDER BY sl_start_time DESC
-			LIMIT 1
-		`
-		countLogSql = `
-			SELECT COUNT(*)
-			FROM owl_schedule_log
-			WHERE sl_sch_id = ?
-		`
+		lockTable OwlSchedule
+		logTable  OwlScheduleLog
+
+		ExpectSuccessSchedule = func(testSchedule *Schedule, testError error) {
+			GinkgoT().Logf("UUID=%v", testSchedule.Uuid)
+			Expect(testError).NotTo(HaveOccurred())
+			Expect(testSchedule.Uuid).NotTo(Equal(uuid.Nil))
+		}
+
+		ExpectLockAndLog = func(expSchedule *Schedule, expNow time.Time, expLogCount int) {
+			selectLockSql := `
+				SELECT *
+				FROM owl_schedule
+				WHERE sch_name = ?
+			`
+			selectLogSql := `
+				SELECT *
+				FROM owl_schedule_log
+				WHERE sl_sch_id = ?
+				ORDER BY sl_start_time DESC
+				LIMIT 1
+			`
+			countLogSql := `
+				SELECT COUNT(*)
+				FROM owl_schedule_log
+				WHERE sl_sch_id = ?
+			`
+			timeThreshold := 500 * time.Millisecond
+
+			By("Check lock")
+			DbFacade.SqlxDbCtrl.Get(&lockTable, selectLockSql, expSchedule.Name)
+			Expect(lockTable.isLocked()).To(BeTrue())
+			Expect(lockTable.LastUpdateTime).To(BeTemporally("~", expNow, timeThreshold))
+
+			By("Check time")
+			DbFacade.SqlxDbCtrl.Get(&logTable, selectLogSql, lockTable.Id)
+			Expect(logTable.Timeout).To(Equal(expSchedule.Timeout))
+			Expect(logTable.StartTime).To(BeTemporally("~", expNow, timeThreshold))
+
+			By("Check log count")
+			var count int
+			DbFacade.SqlxDbCtrl.Get(&count, countLogSql, lockTable.Id)
+			Expect(count).To(Equal(expLogCount))
+		}
 	)
 
 	BeforeEach(func() {
 		defaultSchedule = NewSchedule(defaultScheduleName, defaultTimeout)
-		now = time.Now()
+		defaultNow = time.Now()
 		lockTable = OwlSchedule{}
 		logTable = OwlScheduleLog{}
 	})
@@ -57,82 +82,63 @@ var _ = Describe("Tests AcquireLock(...)", itSkip.PrependBeforeEach(func() {
 
 	Context("Schedule is new", func() {
 		It("should acquire the lock", func() {
-			err := AcquireLock(defaultSchedule, now)
+			err := AcquireLock(defaultSchedule, defaultNow)
 
-			GinkgoT().Logf("UUID=%v", defaultSchedule.Uuid)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(defaultSchedule.Uuid).NotTo(Equal(uuid.Nil))
-
-			By("Check lock")
-			DbFacade.SqlxDbCtrl.Get(&lockTable, selectLockSql, defaultScheduleName)
-			Expect(lockTable.isLocked()).To(BeTrue())
-
-			By("Check time")
-			DbFacade.SqlxDbCtrl.Get(&logTable, selectLogSql, lockTable.Id)
-			Expect(logTable.StartTime).To(Equal(lockTable.LastUpdateTime))
-
-			By("Check log count")
-			var count int
-			DbFacade.SqlxDbCtrl.Get(&count, countLogSql, lockTable.Id)
-			Expect(count).To(Equal(1))
+			ExpectSuccessSchedule(defaultSchedule, err)
+			ExpectLockAndLog(defaultSchedule, defaultNow, 1)
 		})
 	})
 
-	Describe("A schedule has been created", func() {
-		Context("lock is held too long", func() {
-			It("should preempt the lock", func() {
-
-				By("Lock is held by another schedule")
-				s := NewSchedule(defaultScheduleName, 1)
-				err := AcquireLock(s, now)
-				GinkgoT().Logf("UUID=%v", s.Uuid)
-				Expect(err).NotTo(HaveOccurred())
-
-				By("Acquire lock from the stale task")
-				ps := NewSchedule(defaultScheduleName, 0)
-				now = now.Add(2 * time.Second)
-				err = AcquireLock(ps, now)
-				GinkgoT().Logf("UUID=%v", ps.Uuid)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(ps.Uuid).NotTo(Equal(uuid.Nil))
-			})
+	Context("A schedule has been created", func() {
+		JustBeforeEach(func() {
+			_ = AcquireLock(defaultSchedule, defaultNow)
 		})
 
-		Context("lock is held but cannot determine the timeout", func() {
-			var (
-				crasheddefaultScheduleName = "test-schedule-crash"
-				crashedSchedule            = `
-					INSERT INTO owl_schedule (sch_name, sch_lock, sch_modify_time)
-					VALUES ('test-schedule-crash', 1, '2020-01-01 12:00:00')
-				`
-			)
-			BeforeEach(inTx(crashedSchedule))
-
+		Context("lock is held too long", func() {
 			It("should preempt the lock", func() {
-				By("Acquire lock from the crashed task")
-				s := NewSchedule(crasheddefaultScheduleName, 0)
-				err := AcquireLock(s, now)
-				GinkgoT().Logf("UUID=%v", s.Uuid)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(s.Uuid).NotTo(Equal(uuid.Nil))
+				thisTimeout := defaultTimeout + 1
+				ps := NewSchedule(defaultScheduleName, thisTimeout)
+				newCurrent := defaultNow.Add(time.Duration(thisTimeout) * time.Second)
+				err := AcquireLock(ps, newCurrent)
+
+				ExpectSuccessSchedule(ps, err)
+				ExpectLockAndLog(ps, newCurrent, 2)
 			})
 		})
 
 		Context("lock is just held", func() {
 			It("should trigger error", func() {
-				By("Lock is held")
-				s := NewSchedule(defaultScheduleName, 2)
-				err := AcquireLock(s, now)
-				Expect(err).NotTo(HaveOccurred())
+				thisTimeout := defaultTimeout + 1
+				ps := NewSchedule(defaultScheduleName, thisTimeout)
+				err := AcquireLock(ps, defaultNow)
 
-				By("Acquire lock but get error")
-				ps := NewSchedule(defaultScheduleName, 0)
-				err = AcquireLock(ps, now)
-				GinkgoT().Logf("Err=%s", err)
 				Expect(err).To(HaveOccurred())
 				Expect(ps.Uuid).To(Equal(uuid.Nil))
+
+				ExpectLockAndLog(defaultSchedule, defaultNow, 1)
 			})
 		})
+
+		// Context("lock is held but cannot determine the timeout", func() {
+		// 	var (
+		// 		crasheddefaultScheduleName = "test-schedule-crash"
+		// 		crashedSchedule            = `
+		// 				INSERT INTO owl_schedule (sch_name, sch_lock, sch_modify_time)
+		// 				VALUES ('test-schedule-crash', 1, '2020-01-01 12:00:00')
+		// 			`
+		// 	)
+		// 	BeforeEach(inTx(crashedSchedule))
+
+		// 	It("should preempt the lock", func() {
+		// 		By("Acquire lock from the crashed task")
+		// 		s := NewSchedule(crasheddefaultScheduleName, 0)
+		// 		err := AcquireLock(s, defaultNow)
+		// 		GinkgoT().Logf("UUID=%v", s.Uuid)
+		// 		Expect(err).NotTo(HaveOccurred())
+		// 		Expect(s.Uuid).NotTo(Equal(uuid.Nil))
+		// 	})
+		// })
+
 	})
 
 }))
