7ab9e2ee235cc61b6ee60f1a6f30e386a4c885f2
Mike Lue <mikelue@cepave.com>
2018-01-03 18:00:30 +0800

    [OWL-2005] Remove the socket service for receiving data of metric

--- modules/transfer/receiver/receiver.go
+++ modules/transfer/receiver/receiver.go
@@ -2,10 +2,8 @@ package receiver
 
 import (
 	"github.com/Cepave/open-falcon-backend/modules/transfer/receiver/rpc"
-	"github.com/Cepave/open-falcon-backend/modules/transfer/receiver/socket"
 )
 
 func Start() {
 	go rpc.StartRpc()
-	go socket.StartSocket()
 }
